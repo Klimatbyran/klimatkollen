fab18839126eb5e1e61b96c0d2ca671987598315
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-01-10 17:24:14 +0100

    chore: typography

---
+++ components/Typography.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 49px;
+  margin: 0;
+`
+
+export const H2 = styled.h2`
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 39px;
+  margin: 0;
+`
+
+export const H3 = styled.h3`
+  font-weight: bold;
+  font-size: 28px;
+  line-height: 34px;
+  margin: 0;
+`
+
+export const H4 = styled.h4`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 29px;
+  margin: 0;
+`
+
+export const H5 = styled.h5`
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 24px;
+  margin: 0;
+`
+
+export const Paragraph = styled.p`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 19px;
+  margin: 0;
+`
+
+export const ParagraphSmall = styled.p`
+  font-family: Nunito Sans;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+  margin: 0;
+`
+
+export const ParagraphBold = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+  margin: 0;
+`
