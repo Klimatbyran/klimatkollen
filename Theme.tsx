9bcaf7065fff91eac85df830cc5f59887d2c5296
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-01-10 16:50:30 +0100

    feat: color theme

---
+++ Theme.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+
+const colorTheme: DefaultTheme = {
+  main: '#91DFC8',
+  greenGraphOne: '#6BA292',
+  greenGraphTwo: '#94D3C1',
+  greenGraphThree: '#DDF1EB',
+  yellow: '#EFBF17',
+  red: '#EF3054',
+  white: '#FFFFFF',
+  lightGrey: '#F2F2F2',
+  grey: '#6C6C6C',
+  dark: '#424242',
+  black: '#2D2D2D',
+}
+type Props = {
+  children: ReactNode
+}
+
+const Theme = ({ children }: Props) => {
+  return <ThemeProvider theme={colorTheme}>{children}</ThemeProvider>
+}
+
+export default Theme
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Theme from '../Theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Theme>
+      <Component {...pageProps} />
+    </Theme>
+  )
 }
 
 export default MyApp
