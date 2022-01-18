b688653a4c20d3bd91140d338f0b76776d609528
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2022-01-18 16:18:46 +0100

    fix: move component with arrow to the top

--- pages/kommun/[municipality].tsx
+++ pages/kommun/[municipality].tsx
@@ -1,20 +1,20 @@
 import { useRouter } from 'next/router'
 import ShareButton from '../../components/ShareButton'
-import Back from '../../components/Back';
+import Back from '../../components/Back'
 
 const Municipality = () => {
   const router = useRouter()
   const { municipality } = router.query
 
   const handleClick = () => {
-    // Function to handle click on share button    
+    // Function to handle click on share button
   }
 
   return (
     <div>
+      <Back />
       <p>Kommun: {municipality}</p>
       <ShareButton handleClick={handleClick} />
-      <Back />
     </div>
   )
 }
