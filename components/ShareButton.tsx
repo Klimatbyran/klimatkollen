f91d1cd9704bf23140c3ae3ff69181295014e788
Gabrielle Kamph <72319394+gabriellekamph@users.noreply.github.com>
2022-01-18 10:38:48 +0100

    feat: add share button
    * feat: add share button
    
    * feat: add states for share button
    
    * chore: get colors from props.theme and pass click function to button component
    
    * fix: renaming interface
    
    * fix: remove React.FC
    
    * chore: format files
    
    * chore: moved button to municipality
    
    * removed icon from button
    
    * chore: use svgr for svg import

---
+++ components/ShareButton.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+import Icon from '../public/icons/share.svg';
+
+const Button = styled.button`
+  width: 216px;
+  height: 56px;
+  color: ${({ theme }) => theme.black};
+  background: ${({ theme }) => theme.main};
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  border: 0;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  fill: ${({ theme }) => theme.black};
+
+  p {
+    font-weight: bold;
+    font-size: 16px;
+    margin-left: 30%;
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.greenGraphTwo};
+  }
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.white};
+  }
+
+  &:disabled {
+    background: ${({ theme }) => theme.grey};
+    color: ${({ theme }) => theme.white};
+    fill: ${({ theme }) => theme.white};
+  }
+`
+
+type ShareButtonProps = {
+  handleClick: () => void
+}
+
+const ShareButton = ({ handleClick }: ShareButtonProps) => {
+  return (
+    <Button type="button" onClick={handleClick}>
+      <Icon />
+      <p>Dela</p>
+    </Button>
+  )
+}
+
+export default ShareButton
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
   reactStrictMode: true,
 }
