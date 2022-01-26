55f173222bfe3724f08b5a83c700a557ddd99735
Mikaela Lundsgård <mikaelalundsgard@hotmail.com>
2022-01-26 11:01:59 +0100

    removed wrong import

--- pages/api/municipality/[name].tsx
+++ pages/api/municipality/[name].tsx
@@ -1,28 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { MunicipalityData, Municipality, getMunicipalities, getMunicipality } from '../../../utils/emissionService';
-
+import { getMunicipalities, getMunicipality } from '../../../utils/emissionService'
 
 export default function userHandler(req: NextApiRequest, res: NextApiResponse) {
-    
-    const name = req.query.name as string;
-    const method = req.method as string;
-  
-    switch (method) {
-      case 'GET':
+  const name = req.query.name as string
+  const method = req.method as string
 
-        getMunicipality(name).then((municipality) => {
+  switch (method) {
+    case 'GET':
+      getMunicipality(name)
+        .then((municipality) => {
           res.status(200).json(municipality)
-        }).catch((error) => {
+        })
+        .catch((error) => {
           res.status(500).json(error)
         })
-        
-        break
-      // case 'PUT':  
-      //   // Update or create data in your database
-      //   res.status(200).json({ id, name: name || `User ${id}` })
-      //   break
-      default:
-        res.setHeader('Allow', ['GET', 'PUT'])
-        res.status(405).end(`Method ${method} Not Allowed`)
-    }
+
+      break
+    // case 'PUT':
+    //   // Update or create data in your database
+    //   res.status(200).json({ id, name: name || `User ${id}` })
+    //   break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
+}
