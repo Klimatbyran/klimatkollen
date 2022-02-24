2c48519da996f884c18307315c1fb8b360743600
Emil Ahlbäck <emil.ahlback@iteam.se>
2022-02-24 12:40:56 +0100

    Color update, labels

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -7,7 +7,7 @@ import {
   PointElement,
   LineElement,
 } from 'chart.js'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Line } from 'react-chartjs-2'
 
 import styled from 'styled-components'
@@ -17,14 +17,24 @@ Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Filler)
 const RangeContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  padding-left: 12.5rem;
+  padding-left: 11rem;
 `
 
-const Range = styled.input`
+const Range = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Slider = styled.input`
   appearance: slider-vertical;
   writing-mode: bt-lr; // ie and edge
-  width: 0.05rem;
-  margin-right: 2.95rem;
+  width: 3rem;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
 `
 
 type EmissionData = {
@@ -63,7 +73,6 @@ type Props = {
 type Dataset = Array<null | number>
 
 const Graph = ({ step, historical, paris, pledged }: Props) => {
-  const chartRef = useRef()
   const setup = useMemo(
     () => getSetup([historical, paris, pledged]),
     [historical, paris, pledged],
@@ -115,87 +124,9 @@ const Graph = ({ step, historical, paris, pledged }: Props) => {
   // }, [userGraph])
 
   const options = {
-    options: {
-      responsive: true,
-      scales: {
-        x: {
-          min: step > 2 ? 2019 : 1991,
-          max: step > 0 ? 2050 : 2019,
-          grid: {
-            display: false,
-            drawBorder: false,
-          },
-        },
-        y: {
-          grid: {
-            drawBorder: false,
-            display: false,
-          },
-          beginAtZero: true,
-        },
-      },
-    },
+    options: {},
     data: {
       labels: setup.labels,
-      datasets: [
-        {
-          id: 'historical',
-          fill: true,
-          data: historicalDataset,
-          borderWidth: 1,
-          borderColor: 'rgba(239, 94, 48, 1)',
-          backgroundColor: 'rgba(239, 94, 48, 0.3)',
-          pointRadius: 0,
-          tension: 0.5,
-          hidden: false,
-        },
-        {
-          id: 'paris',
-          fill: true,
-          data: parisDataset,
-          borderWidth: 1,
-          borderColor: 'rgba(239, 153, 23, 1)',
-          backgroundColor: 'rgba(239, 153, 23, 0.3)',
-          pointRadius: 0,
-          tension: 0.5,
-          hidden: false,
-        },
-        {
-          id: 'pledge',
-          fill: true,
-          data: pledgeDataset,
-          borderWidth: 1,
-          borderColor: 'rgba(239, 48, 84, 1)',
-          backgroundColor: 'rgba(239, 48, 84, 0.3)',
-          pointRadius: 0,
-          tension: 0.5,
-          hidden: step < 2,
-        },
-        {
-          id: 'usergrap',
-          fill: true,
-          data: (
-            getFillerValues(setup.minYear, setup.adjustableYearStart) as Dataset
-          ).concat(
-            userGraph.reduce(
-              (sum, f) => {
-                const acc = sum.acc * f.change
-                return {
-                  acc,
-                  values: sum.values.concat([f.co2 / acc]),
-                }
-              },
-              { acc: 1, values: [] as Dataset },
-            ).values,
-          ),
-          borderWidth: 1,
-          borderColor: 'rgba(255, 255, 255)',
-          backgroundColor: 'rgba(255, 255, 255, 0.6)',
-          pointRadius: 0,
-          tension: 0,
-          hidden: step < 4,
-        },
-      ],
     },
   }
 
@@ -217,31 +148,174 @@ const Graph = ({ step, historical, paris, pledged }: Props) => {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <Container>
       <Line
-        ref={chartRef}
         datasetIdKey="id"
-        data={options.data}
-        options={options.options}
+        data={{
+          labels: setup.labels,
+          datasets: [
+            {
+              // @ts-ignore
+              id: 'historical',
+              fill: true,
+              data: historicalDataset,
+              borderWidth: 2,
+              borderColor: '#EF5E30',
+              backgroundColor: '#EF5E30',
+              pointRadius: 0,
+              tension: 0.5,
+              hidden: false,
+            },
+            {
+              // @ts-ignore
+              id: 'paris',
+              fill: true,
+              data: parisDataset,
+              borderWidth: 2,
+              borderColor: '#6BA292',
+              backgroundColor: '#91DFC8',
+              pointRadius: 0,
+              tension: 0.5,
+              hidden: false,
+            },
+            {
+              // @ts-ignore
+              id: 'usergrap',
+              fill: true,
+              data: (
+                getFillerValues(setup.minYear, setup.adjustableYearStart) as Dataset
+              ).concat(
+                userGraph.reduce(
+                  (sum, f) => {
+                    const acc = sum.acc * f.change
+                    return {
+                      acc,
+                      values: sum.values.concat([f.co2 / acc]),
+                    }
+                  },
+                  { acc: 1, values: [] as Dataset },
+                ).values,
+              ),
+              borderWidth: 1,
+              borderColor: '#EFBF17',
+              backgroundColor: '#EFBF17',
+              pointRadius: 0,
+              tension: 0.5,
+              hidden: step < 4,
+            },
+            {
+              // @ts-ignore
+              id: 'pledge',
+              fill: true,
+              data: pledgeDataset,
+              borderWidth: 2,
+              borderColor: '#EF3054',
+              backgroundColor: '#542E35',
+              pointRadius: 0,
+              tension: 0.5,
+              hidden: step < 2,
+            },
+          ],
+        }}
+        options={{
+          responsive: true,
+          scales: {
+            x: {
+              min: step > 2 ? 2019 : setup.minYear,
+              max: step > 0 ? 2050 : 2019,
+              grid: {
+                display: true,
+                drawBorder: false,
+                color: 'rgba(255, 255, 255, 0.2)',
+                drawTicks: false,
+              },
+              ticks: {
+                font: {
+                  family: 'Helvetica Neue',
+                  size: 15,
+                  weight: '300',
+                },
+                color: 'white',
+                align: 'center',
+                callback: (tickValue) => {
+                  console.log(tickValue)
+                  const idx = tickValue as number
+                  console.log(
+                    idx,
+                    setup.labels[idx],
+                    idx % 2 === 0,
+                    idx % 2 === 0 ? setup.labels[idx] : '',
+                  )
+                  return idx % 2 === 0 ? setup.labels[idx] : ''
+                },
+              },
+            },
+            y: {
+              suggestedMax: 150_000,
+              grid: {
+                drawBorder: false,
+                display: false,
+              },
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Tusen ton CO₂',
+                align: 'center',
+                font: {
+                  family: 'Helvetica Neue',
+                  size: 15,
+                  weight: '300',
+                },
+                color: 'white',
+              },
+              ticks: {
+                stepSize: 50_000,
+                font: {
+                  family: 'Helvetica Neue',
+                  size: 15,
+                  weight: '300',
+                },
+                color: 'white',
+                callback: (a, _idx) => {
+                  return ((a as number) / 1000).toString()
+                },
+              },
+            },
+          },
+        }}
       />
       {step > 3 && (
-        <RangeContainer>
-          {userGraph.map((value, i) => (
-            <Range
-              key={i}
-              min={1}
-              max={2}
-              step={0.01}
-              value={value.change}
-              type="range"
-              // @ts-ignore - this is for firefox :*(
-              orient="vertical"
-              onChange={(e) => handleYearChange(i, parseFloat(e.target.value))}
-            />
-          ))}
-        </RangeContainer>
+        <>
+          <RangeContainer>
+            {userGraph.map((value, i) => (
+              <Range
+                key={i}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}>
+                <Slider
+                  min={1}
+                  max={2}
+                  step={0.01}
+                  value={value.change}
+                  type="range"
+                  // @ts-ignore - this is for firefox :*(
+                  orient="vertical"
+                  onChange={(e) => handleYearChange(i, parseFloat(e.target.value))}
+                />
+                <label>{100 - Math.round(100 / value.change)}%</label>
+              </Range>
+            ))}
+          </RangeContainer>
+          <p>
+            Med hjälp av reglagen så styr du hur stora utsläppsminskningar man behöver
+            göra per mandatperiod för att nå Parisavtalet.
+          </p>
+        </>
       )}
-    </div>
+    </Container>
   )
 }
 
