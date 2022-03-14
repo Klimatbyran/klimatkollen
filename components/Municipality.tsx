4025c10e5401c2511c34061c6678cbb1e08e210f
Johanna Grahn <johanna.grahn@iteam.se>
2022-03-14 14:58:23 +0100

    Now showing numbers in the correct step

--- components/Municipality.tsx
+++ components/Municipality.tsx
@@ -532,25 +532,25 @@ const Municipality = (props: Props) => {
                   Historiska utsläpp
                 </Legend>
               )}
-              {step > 1 && (
+              {step > 0 && (
                 <Legend>
                   <Circle color="#6BA292" />
                     Parisavtalet
-                    {step > 3 && (
+                    {step > 2 && (
                       ": " + Math.round(totalBudget/1000) + " kt CO₂"
                     )}
                 </Legend>
               )}
-              {step > 2 && (
+              {step > 1 && (
                 <Legend>
                   <Circle color="#EF3054" />
                   Fortsätta som idag
-                  {step > 3 && (
+                  {step > 2 && (
                     ": " + Math.round(totalTrend/1000) + " kt CO₂"
                   )}
                 </Legend>
               )}
-              {step > 3 && (
+              {step > 2 && (
                 <Legend>
                   <Line color="rgb(239, 191, 23)" />
                   Din plan: {Math.round(userTotal/1000)} kt CO₂
