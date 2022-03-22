e3c3205be82d6df0b15d66ac199be630bf637ee4
Emil Ahlbäck <emil.ahlback@iteam.se>
2022-03-22 13:52:28 +0100

    Prelooading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,34 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* <link
-          href="/Roboto/Roboto-Regular.ttf"
+        <link
+          href="/roboto/roboto-v29-latin-300.woff2"
           rel="preload"
           as="font"
-          type="font/ttf"
+          type="font/woff2"
           crossOrigin="anonymous"
-        /> */}
+        />
+        <link
+          href="/roboto/roboto-v29-latin-regular.woff2"
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/roboto/roboto-v29-latin-500.woff2"
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/roboto/roboto-v29-latin-700.woff2"
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
