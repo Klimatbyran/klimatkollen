64eba10a97f18c00adc86f933a60ff848843cd25
Rickard Natt och Dag <rdag@invidi.com>
2022-03-28 11:38:50 +0200

    test: update wikiDataService to use vitest

--- __tests__/utils/wikiDataService.test.tsx
+++ __tests__/utils/wikiDataService.test.tsx
@@ -1,22 +1,23 @@
-
 import { WikiDataService } from '../../utils/wikiDataService'
+
+vi.mock('axios')
 
 describe('#getMunicipalityByName()', () => {
   it('should get municipality', () => {
-
-    jest.mock('axios')
-    
-    return new WikiDataService().getMunicipalityByName('Uddevalla')
-        .then(municipality => {
-            
-            expect(municipality).toBeDefined()
-            expect(municipality.Population).toBe(57011)
-            expect(municipality.CoatOfArmsImage.ImageUrl).toEqual("https://upload.wikimedia.org/wikipedia/commons/1/10/Uddevalla_vapen.svg")
-            expect(municipality.CoatOfArmsImage.Description).toEqual("")
-            expect(municipality.Image.ImageUrl).toEqual("https://upload.wikimedia.org/wikipedia/commons/3/3b/Rådhuset_Uddevalla.JPG")
-            expect(municipality.Image.Description).toEqual("")
-
-        })
-        
+    return new WikiDataService()
+      .getMunicipalityByName('Uddevalla')
+      .then((municipality) => {
+        expect(municipality).toBeDefined()
+        expect(municipality.Population).toBe(57011)
+        expect(municipality.CoatOfArmsImage.ImageUrl).toEqual(
+          'https://upload.wikimedia.org/wikipedia/commons/1/10/Uddevalla_vapen.svg',
+        )
+        expect(municipality.CoatOfArmsImage.Description).toEqual('')
+        expect(municipality.Image.ImageUrl).toEqual(
+          'https://upload.wikimedia.org/wikipedia/commons/3/3b/Rådhuset_Uddevalla.JPG',
+        )
+        expect(municipality.Image.Description).toEqual('')
+      })
   })
 })
+
