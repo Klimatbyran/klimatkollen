5ffbdf16eb88e4fa8c713e82834fb383d2edc76a
Elvira Boman <>
2022-03-28 11:28:23 +0200

    added constraints to map viewState so user can't toss map out of wrapper

--- components/Map.tsx
+++ components/Map.tsx
@@ -82,13 +82,13 @@ const Map = ({ emissionsLevels, setSelected, children }: Props) => {
   const [municipalityData, setMunicipalityData] = useState<any>({})
   const router = useRouter()
 
-  useMemo (() => {
+  useMemo(() => {
     axios.get('/api/map').then(res => {
       setMunicipalityData(res.data)
     })
   }, [])
-   
-  const municipalityLines = municipalityData?.features?.map(({ geometry, properties } : {geometry: any, properties: any}) => {
+
+  const municipalityLines = municipalityData?.features?.map(({ geometry, properties }: { geometry: any, properties: any }) => {
     const name = replaceLetters(properties.name)
     const emissions = emissionsLevels.find((e) => e.name === name)?.emissions
     return {
@@ -150,17 +150,11 @@ const Map = ({ emissionsLevels, setSelected, children }: Props) => {
           if (name) router.push(`/kommun/${replaceLetters(name).toLowerCase()}`)
         }}
         layers={[kommunLayer]}
-        // onViewStateChange={({ viewState }) => {
-        //   viewState.longitude = Math.min(
-        //     MAP_RANGE.lon[1],
-        //     Math.max(MAP_RANGE.lon[0], viewState.longitude),
-        //   )
-        //   viewState.latitude = Math.min(
-        //     MAP_RANGE.lat[1],
-        //     Math.max(MAP_RANGE.lat[0], viewState.latitude),
-        //   )
-        //   return viewState
-        // }}
+        onViewStateChange={({ viewState }) => {
+          viewState.longitude = Math.min(MAP_RANGE.lon[1], Math.max(MAP_RANGE.lon[0], viewState.longitude))
+          viewState.latitude = Math.min(MAP_RANGE.lat[1], Math.max(MAP_RANGE.lat[0], viewState.latitude))
+          return viewState
+        }}
       />
       {children}
     </DeckGLWrapper>
