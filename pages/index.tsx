69e5dd5744bf54d2b89ebab3e1a136584a68c213
Elvira Boman <>
2022-03-29 14:04:56 +0200

    new fix w/o line breaks in percent + smaller font for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,10 +90,11 @@ const FlexCenter = styled.div`
 
 const StyledParagraph = styled(Paragraph)`
   z-index: 1;
-  max-width: 6.7em;
+  width: 5em;
+  font-size: 0.7em;
 
   @media only screen and (${devices.tablet}) {
-    max-width: 20em;
+    font-size: 1em;
   }
 `
 
