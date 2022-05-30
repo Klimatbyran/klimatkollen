b976fcf3895636b629c2da6d68f17606123f73d2
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-05-30 14:37:57 +0200

    fix: adjust heading sizes

--- components/Typography.ts
+++ components/Typography.ts
@@ -2,36 +2,36 @@ import styled from 'styled-components'
 
 export const H1 = styled.h1`
   font-weight: bold;
-  font-size: 40px;
-  line-height: 49px;
+  font-size: 48px;
+  line-height: 1.25;
   margin: 0;
 `
 
 export const H2 = styled.h2`
   font-weight: bold;
-  font-size: 32px;
-  line-height: 39px;
+  font-size: 30px;
+  line-height: 1.25;
   margin: 0;
 `
 
 export const H3 = styled.h3`
   font-weight: bold;
-  font-size: 28px;
-  line-height: 34px;
+  font-size: 24px;
+  line-height: 1.25;
   margin: 0;
 `
 
 export const H4 = styled.h4`
   font-weight: bold;
-  font-size: 24px;
-  line-height: 29px;
+  font-size: 20px;
+  line-height: 1.25;
   margin: 0;
 `
 
 export const H5 = styled.h5`
   font-weight: bold;
-  font-size: 20px;
-  line-height: 24px;
+  font-size: 18px;
+  line-height: 1.25;
   margin: 0;
 `
 
