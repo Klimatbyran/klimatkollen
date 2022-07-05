4a17751bb83e75fbf0cca1a4924909e0cc5a4410
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-07-05 15:02:54 +0200

    feat: add metadata to /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import { GetServerSideProps } from 'next'
+import MetaTags from '../components/MetaTags'
 
 export default function Index() {
-  return <></>
+  return (
+    <>
+      <MetaTags
+        title="Klimatkollen — Få koll på Sveriges klimatomställning"
+        description="Hur går det med utsläppsminskningarna i Sverige och i din kommun? Minskar eller ökar klimatutsläppen? Klarar vi Parisavtalet?"
+      />
+    </>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
