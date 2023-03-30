8c69d293ed95389186570725081de2b6907f41a4
Elvira <elvbom@users.noreply.github.com>
2023-03-30 07:38:27 +0200

    Fix index test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -12,6 +12,8 @@ vi.mock('../public/icons/list.svg', () => ({ default: 'svg' }))
 vi.mock('../public/icons/map.svg', () => ({ default: 'svg' }))
 vi.mock('../public/icons/info.svg', () => ({ default: () => 'svg' }))
 vi.mock('../components/Map', () => ({ default: () => <div /> }))
+vi.mock('../components/ComparisonTable', () => ({ default: () => <div /> }))
+
 vi.mock('next/router')
 
 const setup = () => {
