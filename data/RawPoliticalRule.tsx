d00419999fa242d8a340e9652473318ff4d86fc7
Elvira <elvbom@users.noreply.github.com>
2023-04-03 14:27:47 +0200

    minor fix

--- data/RawPoliticalRule.tsx
+++ data/RawPoliticalRule.tsx
@@ -1,5 +1,5 @@
 import rawPoliticalRule from './raw-political-rule.json'
 
-const RawPoliticalRule = rawPoliticalRule as {kommun: string, styre: string}[]
+const RawPoliticalRule = rawPoliticalRule as {kommun: string, styre: string, other: string}[]
 
 export default RawPoliticalRule
