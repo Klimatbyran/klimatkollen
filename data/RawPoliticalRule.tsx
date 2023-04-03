fef7e4a8ce1415b1bc65df148e2b1715b732df72
Elvira <elvbom@users.noreply.github.com>
2023-04-03 14:23:26 +0200

    update political rule

---
+++ data/RawPoliticalRule.tsx
@@ -0,0 +1,5 @@
+import rawPoliticalRule from './raw-political-rule.json'
+
+const RawPoliticalRule = rawPoliticalRule as {kommun: string, styre: string}[]
+
+export default RawPoliticalRule
--- data/political_rule.py
+++ data/political_rule.py
@@ -5,6 +5,21 @@ import numpy as np
 import pandas as pd
 
 path_political_rule = 'politicalRule2022.xlsx'
-df_raw = pd.read_excel(path_political_rule)
+df = pd.read_excel(path_political_rule)
 
-print(df_raw.head())  # fixme fortsätt här
+df['Kommun'] = df['Unnamed: 1']
+df.loc[df['Kommun'] == 'Falu', 'Kommun'] = 'Falun'
+df['Other'] = df['Annat parti, ange vilket eller vilka']
+df = df.filter(['Kommun', 'SD 2022', 'M 2022', 'KD 2022', 'L 2022',
+               'C 2022', 'MP 2022', 'S 2022', 'V 2022', 'ÖP 2022', 'Other'])
+
+RawPoliticalRule = []
+for i, row in df.iterrows():
+    kommun = row['Kommun']
+    styre = ','.join([str(row[col]) if not pd.isna(row[col])
+                     else '' for col in df.columns[1:-1]])
+    other = row['Other'] if not pd.isna(row['Other']) else ''
+    RawPoliticalRule.append({'kommun': kommun, 'styre': styre, 'other': other})
+
+with open('raw-political-rule.json', 'w') as f:
+    json.dump(RawPoliticalRule, f)
