baa085ed03594d1658b724886ec2862facabdcf6
Elvira <elvbom@users.noreply.github.com>
2023-04-06 11:27:39 +0200

    update dataset descriptions

--- data/dataset_description.tsx
+++ data/dataset_description.tsx
@@ -1,7 +1,16 @@
 export const dataSetDescriptions = {
   'Utsläppen': {
     'heading': 'Utsläppsförändring sedan Parisavtalet',
-    'body': 'På kartan och i listan visas genomsnittlig årlig förändring av kolidioxidutsläppen i Sveriges kommuner sedan Parisavtalet 2015.',
+    'body':
+      <>
+        På kartan och i listan visas genomsnittlig årlig förändring av kolidioxidutsläppen i Sveriges kommuner sedan Parisavtalet 2015.
+        Källa:{' '}
+        <a href='https://nationellaemissionsdatabasen.smhi.se/'
+          target='_blank'
+          rel='noreferrer'>
+          SMHI
+        </a>
+      </>,
     'boundaries': [0, -0.01, -0.02, -0.03, -0.10],
     'labels': ['0% +', '0–1%', '1–2%', '2–3%', '3–10%', '10–15%'],
     'labelRotateUp': [true, false, false, false, false, false],
@@ -9,10 +18,19 @@ export const dataSetDescriptions = {
   },
   'Elbilarna': {
     'heading': 'Ökningstakt andel elbilar sedan Parisavtalet',
-    'body': 'På kartan och i listan visas ökningstakten i kommunerna för andel nyregistrerade laddbara bilar 2015–2022, angivet i procentenheter per år.',
+    'body': 
+    <>
+      På kartan och i listan visas ökningstakten i kommunerna för andel nyregistrerade laddbara bilar 2015–2022, angivet i procentenheter per år.
+      Källa:{' '}
+      <a href='https://www.trafa.se/vagtrafik/fordon/'
+        target='_blank'
+        rel='noreferrer'>
+        Trafikanalys
+      </a>
+    </>,
     'boundaries': [0.04, 0.05, 0.06, 0.07, 0.08],
-    'labels': ['4% –', '4–5%', '5–6%', '6–7%', '7–8%', '8% +'],
+    'labels': ['4 -', '4–5', '5–6', '6–7', '7–8', '8 +'],
     'labelRotateUp': [true, true, true, true, true, true],
-    'tooltip': 'Ökningstakten för andelen nyregistrerade laddbara bilar sedan Parisavtalet 2015 i procentenheter per år. Källa: Trafikanalys.',
+    'tooltip': 'Ökningstakten för andelen nyregistrerade laddbara bilar sedan Parisavtalet 2015 i procentenheter per år',
   }
 }
