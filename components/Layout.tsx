1e868c394e44182c65c7f6fdddadcd1b6c894b7b
Elvira <elvbom@users.noreply.github.com>
2023-10-10 20:57:32 +0200

    Fix/update margins (#276)
    * fix for margins on mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,14 +3,15 @@ import styled from 'styled-components'
 import { H1 } from './Typography'
 import VisuallyHidden from './VisuallyHidden'
 import Header from './Header'
+import { devices } from '../utils/devices'
 
 const Main = styled.main`
   display: flex;
   flex-direction: column;
-  margin: 56px auto 32px auto;
+  margin: 80px auto 32px auto;
 
-  @media only screen and (max-width: 600px) {
-    margin: 32px auto 16px auto;
+  @media only screen and ${devices.tablet} {
+    margin: 96px auto 32px auto;
   }
 `
 
