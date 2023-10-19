68a0b3b3fe1aa0baefcc492effb4e14ae0bebfaf
Elvira <elvbom@users.noreply.github.com>
2023-10-19 08:45:50 +0200

    Feat/add new partner researchers desk (#282)
    * researchers desk logo added
    
    * add styling for mobile
    
    * cleanup of partner logos

--- components/Footer/FooterPartners.tsx
+++ components/Footer/FooterPartners.tsx
@@ -2,16 +2,20 @@ import styled from 'styled-components'
 import { devices } from '../../utils/devices'
 
 const PartnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(90px, 1fr));
+  grid-gap: 16px;
+  justify-items: center;
   align-items: center;
   width: 100%;
   padding: 8px 16px;
   margin-bottom: 48px;
 
   @media only screen and (${devices.tablet}) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
     padding: 16px 32px;
   }
 `
@@ -21,10 +25,18 @@ function Partners() {
     <PartnerContainer>
       <a href="https://postkodstiftelsen.se/" target="_blank" rel="noreferrer">
         <img
-          src="/icons/postkodstiftelsen.svg"
+          src="/partners/postkodstiftelsen.svg"
           width={90}
           height="auto"
           alt="Postkodstiftelsen logo"
+        />
+      </a>
+      <a href="https://www.climateview.global/" target="_blank" rel="noreferrer">
+        <img
+          src="/partners/climateview.svg"
+          width={90}
+          height="auto"
+          alt="ClimateView logo"
         />
       </a>
       <a href="https://www.wwf.se/" target="_blank" rel="noreferrer">
@@ -35,17 +47,17 @@ function Partners() {
           alt="WWF logo"
         />
       </a>
-      <a href="https://www.climateview.global/" target="_blank" rel="noreferrer">
+      <a href="https://researchersdesk.se/" target="_blank" rel="noreferrer">
         <img
-          src="/icons/climateview.svg"
-          width={90}
-          height="auto"
-          alt="ClimateView logo"
+          src="/partners/researchersdesk-logo.svg"
+          width="auto"
+          height={60}
+          alt="Researchers desk logo"
         />
       </a>
       <a href="https://www.klimatklubben.se/" target="_blank" rel="noreferrer">
         <img
-          src="/icons/klimatklubben.svg"
+          src="/partners/klimatklubben.svg"
           width="auto"
           height={45}
           alt="Klimatklubben logo"
