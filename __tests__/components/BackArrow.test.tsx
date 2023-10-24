988d2c53a8050247a850262d7fc1e09334a5a408
Elvira <elvbom@users.noreply.github.com>
2023-10-24 21:20:14 +0200

    add test backarrow

---
+++ __tests__/components/BackArrow.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import BackArrow from '../../components/BackArrow'
+
+vi.mock('../../public/icons/arrow-left.svg', () => ({ default: 'svg' }))
+
+describe('BackArrow Component', () => {
+  it('should render the BackArrow with the correct route', () => {
+    const route = '/some-route'
+    render(<BackArrow route={route} />)
+
+    const linkElement = screen.getByRole('link')
+
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toHaveAttribute('href', route)
+  })
+})
