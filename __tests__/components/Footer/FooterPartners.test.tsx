c3ce54ee6d48a7e33e3ebb2c3e7b2c6e2221bdbd
Elvira <elvbom@users.noreply.github.com>
2023-10-26 14:41:37 +0200

    add tests for footer components

---
+++ __tests__/components/Footer/FooterPartners.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Partners from '../../../components/Footer/FooterPartners'
+
+describe('Partners component', () => {
+  beforeEach(() => {
+    render(<Partners />)
+  })
+
+  it('should render all partner links', () => {
+    const links = screen.getAllByRole('link')
+    expect(links.length).toBe(5)
+
+    // Checking partner URL
+    expect(links[0]).toHaveAttribute('href', 'https://postkodstiftelsen.se/')
+    expect(links[1]).toHaveAttribute('href', 'https://www.climateview.global/')
+    expect(links[2]).toHaveAttribute('href', 'https://www.wwf.se/')
+    expect(links[3]).toHaveAttribute('href', 'https://researchersdesk.se/')
+    expect(links[4]).toHaveAttribute('href', 'https://www.klimatklubben.se/')
+  })
+
+  it('should render all partner images with correct alt text', () => {
+    const images = screen.getAllByRole('img')
+    expect(images.length).toBe(5)
+
+    // Checking image alt text
+    expect(images[0]).toHaveAttribute('alt', 'Postkodstiftelsen logo')
+    expect(images[1]).toHaveAttribute('alt', 'ClimateView logo')
+    expect(images[2]).toHaveAttribute('alt', 'WWF logo')
+    expect(images[3]).toHaveAttribute('alt', 'Researchers desk logo')
+    expect(images[4]).toHaveAttribute('alt', 'Klimatklubben logo')
+  })
+})
--- components/Footer/FooterNewsletterSubscribe.tsx
+++ components/Footer/FooterNewsletterSubscribe.tsx
@@ -3,13 +3,13 @@ import NewsletterForm from './FooterNewsletterForm'
 
 function NewsletterSubscribe() {
   const MAILCHIMP_URL = process.env.NEXT_PUBLIC_MAILCHIMP_URL
-  if (typeof MAILCHIMP_URL === 'undefined') throw new Error('Must have a mailchimp URL')
+  if (MAILCHIMP_URL === undefined) throw new Error('Must have a mailchimp URL')
 
   return (
     <MailchimpSubscribe
       url={MAILCHIMP_URL}
       render={({ subscribe, status }) => (
-        <NewsletterForm
+        <NewsletterForm 
           status={status}
           onValidated={(formData) => subscribe(formData)}
         />
