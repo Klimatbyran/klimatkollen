a82a464679aaf121c29c4f75ca209f4882ce1a13
Elvira <elvbom@users.noreply.github.com>
2023-11-02 16:47:36 +0100

    Hotfix/mend test partner test (#296)

--- __tests__/components/Footer/FooterPartners.test.tsx
+++ __tests__/components/Footer/FooterPartners.test.tsx
@@ -9,23 +9,23 @@ describe('Partners component', () => {
 
   it('should render all partner links', () => {
     const links = screen.getAllByRole('link')
-    expect(links.length).toBe(5)
+    expect(links.length).toBe(4)
 
     // Checking partner URL
     expect(links[0]).toHaveAttribute('href', 'https://postkodstiftelsen.se/')
     expect(links[1]).toHaveAttribute('href', 'https://www.climateview.global/')
-    expect(links[3]).toHaveAttribute('href', 'https://researchersdesk.se/')
-    expect(links[4]).toHaveAttribute('href', 'https://www.klimatklubben.se/')
+    expect(links[2]).toHaveAttribute('href', 'https://researchersdesk.se/')
+    expect(links[3]).toHaveAttribute('href', 'https://www.klimatklubben.se/')
   })
 
   it('should render all partner images with correct alt text', () => {
     const images = screen.getAllByRole('img')
-    expect(images.length).toBe(5)
+    expect(images.length).toBe(4)
 
     // Checking image alt text
     expect(images[0]).toHaveAttribute('alt', 'Postkodstiftelsen logo')
     expect(images[1]).toHaveAttribute('alt', 'ClimateView logo')
-    expect(images[3]).toHaveAttribute('alt', 'Researchers desk logo')
-    expect(images[4]).toHaveAttribute('alt', 'Klimatklubben logo')
+    expect(images[2]).toHaveAttribute('alt', 'Researchers desk logo')
+    expect(images[3]).toHaveAttribute('alt', 'Klimatklubben logo')
   })
 })
