89cfe96f93da67bd8158a6a3718f4fb1df722702
Elvira <elvbom@users.noreply.github.com>
2023-11-16 20:47:16 +0100

    start refactoring

--- data/solutions/cars/car_data_calculations.py
+++ data/solutions/cars/car_data_calculations.py
@@ -25,16 +25,19 @@ def set_dataframe_columns(df, header_row):
     df.columns = df.iloc[header_row]
     return df.drop(range(header_row + 4))
 
+
 def clean_municipality_data(df):
     df['Kommun'] = df['Kommun'].str.strip()
     df = df.drop(df[df['Kommun'].isin(['Okänd Kommun   ', 'OKÄND KOMMUN', 'OKÄND KOMMUN1)'])].index)
     return df.dropna(subset=['Kommun'])
+
 
 def process_common_data(df, year):
     df['electricity'] = df['El'].replace([' –', '–'], 0).astype(float)
     df['plugIn'] = df['Laddhybrider'].replace([' –', '–'], 0).astype(float)
     df[year] = df['electricity'] + df['plugIn']
     return df.filter(['Kommun', year], axis=1)
+
 
 def load_and_process_trafa_data(path, year):
     # Define sheet names and header rows for each year
@@ -64,9 +67,9 @@ def load_and_process_trafa_data(path, year):
     # Process common data and return
     return process_common_data(df, year)
 
+
 def get_electric_car_change(df):
     # LOAD AND PREP DATA ON CHANGE RATE OF PERCENTAGE OF NEWLY REGISTERED RECHARGABLE CARS PER MUNICIPALITY AND YEAR
-
     df_raw_cars = pd.read_excel(PATH_CARS_DATA)
 
     df_raw_cars.columns = df_raw_cars.iloc[1]  # name columns after row
@@ -85,123 +88,58 @@ def get_electric_car_change(df):
     return df
 
 
-def get_population_data():
-    # Use if population data is needed again at a later stage
-    # Load population data, set correct column headers and drop unwanted rows
-    df_population = pd.read_excel(PATH_POPULATION)
-    df_population.columns = df_population.iloc[4]
-    df_population = df_population[5:]
-    df_population.reset_index(drop=True, inplace=True)  # reset index
-
-    # Keep only the columns with years between 2013 and 2022 to harmonize with PowerCircle data
-    df_population_filtered = df_population[[
-        'Kommun'] + [year for year in year_range]]
-    df_population_filtered.columns = [int(col) if isinstance(
-        col, float) else col for col in df_population_filtered.columns]
-
-    return df_population_filtered
-
-
-def convert_column_name_to_float(df, year_range):
+def convert_column_to_float(df, year_range):
     for year in year_range:
         df.rename(columns={f'{year}': float(year)}, inplace=True)
     return df
 
 
-def convert_column_to_float(df, year_range):
+def load_charging_point_data():
+    df_charging_raw = pd.read_csv(PATH_CHARGING_POINT)
+    df_charging_filtered = df_charging_raw[df_charging_raw['år-månad'].str.endswith('-12')].reset_index(drop=True)
+    df_charging_filtered.rename(columns={'år-månad': 'year'}, inplace=True)
+    df_charging_filtered['year'] = df_charging_filtered['year'].str[:-3]
+    return df_charging_filtered
+
+
+def pivot_charging_data(df_charging_filtered):
+    df_charging_melted = pd.melt(df_charging_filtered, id_vars=['year'], var_name='Kommun', value_name='Value')
+    df_charging_pivoted = df_charging_melted.pivot(index='Kommun', columns='year', values='Value').reset_index()
+    df_charging_pivoted['Kommun'] = df_charging_pivoted['Kommun'].str.title()
+    return df_charging_pivoted
+
+
+def merge_trafa_charging_data(df_result, df_trafa, df_charging, year_range):
+    df_result = df_result.merge(df_trafa, on='Kommun', how='left')
+    df_result = df_result.merge(df_charging, on='Kommun', how='left')
     for year in year_range:
-        df[year] = df[year].astype(float)
-    return df
-
-
-def calculate_average_yearly_change(df, year_range):    
-    # Calculate yearly changes for each Kommun
-    df_yearly_changes = df[year_range].diff(axis=1)
-
-    # Calculate the average yearly change
-    # We use 'iloc[:, 1:]' to exclude the first year (2015) as it will have NaN values after diff()
-    df['cpevChangeAverage'] = df_yearly_changes.iloc[:, 1:].mean(axis=1)*100
-
-    print(df[df['Kommun'] == 'Sorsele'])
-    print(df_yearly_changes.iloc[196])
-
-    return df
+        c = df_result[f'{year}.0_y']
+        ev = df_result[f'{year}_x']
+        df_result[year] = c / ev
+    return df_result
 
 
 def charging_point_calculations():
     year_range = range(2015, 2023)
     df_result = pd.DataFrame()
 
-    # Load charging point data, filter for december and set correct column headers
-    df_charging_raw = pd.read_csv(PATH_CHARGING_POINT)
-    df_charging_filtered = df_charging_raw[df_charging_raw['år-månad'].str.endswith(
-        '-12')].reset_index(drop=True)
-    df_charging_filtered.rename(columns={'år-månad': 'year'}, inplace=True)
-    df_charging_filtered['year'] = df_charging_filtered['year'].str[:-3]
-    df_charging_melted = pd.melt(df_charging_filtered, id_vars=[
-                                'year'], var_name='Kommun', value_name='Value')
-    df_charging_pivoted = df_charging_melted.pivot(
-        index='Kommun', columns='year', values='Value').reset_index()
-    
-    df_charging_pivoted['Kommun'] = df_charging_pivoted['Kommun'].str.title()
-    df_result['Kommun'] = df_charging_pivoted['Kommun'] # Assign Kommun column to result df
-    df_result = df_result.drop(index=range(290, len(df_result)))
+    # Load and process charging point data
+    df_charging_filtered = load_charging_point_data()
+    df_charging_pivoted = pivot_charging_data(df_charging_filtered)
+    df_charging_float = convert_column_to_float(df_charging_pivoted, year_range)
 
-    df_charging_renamed = convert_column_name_to_float(df_charging_pivoted, year_range)
-    df_charging_float = convert_column_to_float(df_charging_renamed, year_range)
-
-    # print(df_charging_float.head(10))   
-
-    # Remove duplicate occurrences of Pajala
-    df_charging_duplicates_removed = df_charging_float.drop(index=172, inplace=True)
-
-    # Load and process Trafa data for each year
+    # Load and process TRAFA data and merge with charging data
     for year, path in trafa_paths.items():
-        df_year = load_and_process_trafa_data(path, year)
-        df_result = df_result.merge(df_year, on='Kommun', how='left')
-        # df_result[year] = df_charging_float[year]/df_year[year]
+        df_trafa = load_and_process_trafa_data(path, year)
+        df_result = merge_trafa_charging_data(df_result, df_trafa, df_charging_float, year_range)
 
-    df_result = df_result.merge(df_charging_float, on='Kommun', how='left')
-
-    for year in year_range:
-        c = df_result[f'{year}.0_y']
-        ev = df_result[f'{year}_x']
-        df_result[year] = c/ev
-        
+    # Calculate the average yearly change
     df_result = calculate_average_yearly_change(df_result, year_range)
     df_result = df_result.filter(['Kommun', 'cpevChangeAverage'], axis=1)
+
+    # Save the result
     df_result.to_excel('output/cpev.xlsx')
 
-        # if 'Kommun' not in all_years_data.columns:
-        #     all_years_data = df_year
-        # else:
-        #     all_years_data = all_years_data.merge(df_year, on='Kommun', how='left')
-
-
-    # # Pivot to get Kommun as rows and years as columns
-    # df_final = df_all_years.pivot_table(index='Kommun', columns='Year', values='totalElectric').reset_index()
-
-    # df_plugin_cars = df_plugin_cars.filter(['Kommun', 'totalElectric'], axis=1)
-
-    # df_result = df_result.merge(df_plugin_cars, on='Kommun', how='left')
-
-    # print(df_result.head(10))   
-
-    # cpev = {}
-    # for year in year_range:
-    #     cpev[year] = df_result[year] / df_result['totalElectric'][year]
-
-    # df_result_filtered = df_result.filter(
-    #     ['Kommun', 'CPEV'], axis=1)
-    
-    # # df_result_filtered['ChargingPointsYearlyAverage'] = df_result_filtered['ChargingPointsPerYear'].apply(
-    # #     # fixme fortsätt här
-    # #     lambda x: (x[6] - x[0]) / len(x) if len(x) > 0 else 0
-    # # )
-
-    # print(df_result_filtered.head(10))
-
-    # df_result_filtered.to_excel('output/cpev.xlsx')
     # smallest = df_result_filtered['CPEV'].min()
     # largest = df_result_filtered['CPEV'].max()
     # mean = df_result_filtered['CPEV'].mean()
