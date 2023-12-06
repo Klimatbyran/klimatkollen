c418031166dca742f194ec54c780045182641143
Elvira <elvbom@users.noreply.github.com>
2023-12-06 15:15:17 +0100

    change share pic to board impact challenge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -65,11 +65,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           sizes="512x512"
           href="/favicons/favicon-512x512.png"
         />
-        <meta property="og:image" content="https://klimatkollen.se/images/social-picture.png" />
+        <meta property="og:image" content="https://klimatkollen.se/board/impact_challenge.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:image"
-          content="https://klimatkollen.se/images/social-picture.png"
+          content="https://klimatkollen.se/board/impact_challenge.png"
         />
       </Head>
       <Theme>
