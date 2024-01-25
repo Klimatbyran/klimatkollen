19c4a056f0d9688a77b95929cc5070ffb1a7650d
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2024-01-25 11:22:31 +0100

    Add paris path test

--- data/tests/test_emission_calculations.py
+++ data/tests/test_emission_calculations.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
+import ast
 import unittest
 import datetime
 import pandas as pd
 
 from issues.emissions.emission_data_calculations import get_n_prep_data_from_smhi, deduct_cement, calculate_municipality_budgets, calculate_paris_path, calculate_trend, calculate_change_percent, calculate_budget_runs_out
+from tests.utilities import prep_dict_for_compare
 
-LAST_YEAR_IN_SMHI_DATA = 2021
-
-
-class TestChargingPointCalculations(unittest.TestCase):
+class TestEmissionCalculations(unittest.TestCase):
     
     def test_get_n_prep_data_from_smhi(self):
         path_input_df = 'tests/reference_dataframes/df_municipalities.xlsx'
@@ -36,6 +35,28 @@ class TestChargingPointCalculations(unittest.TestCase):
         df_result = calculate_municipality_budgets(df_input)
         df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
         pd.testing.assert_frame_equal(df_result, df_expected, check_dtype=False)
+    
+    def test_calculate_paris_path(self):
+        path_input_df = 'tests/reference_dataframes/df_budgeted.xlsx'
+        path_expected_df = 'tests/reference_dataframes/df_paris.xlsx'
+        
+        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_result = calculate_paris_path(df_input)
+        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        
+        series_result = df_result['parisPath']
+        series_expected = df_expected['parisPath']
+        
+        series_result = [prep_dict_for_compare(serie) for serie in series_result]
+        series_expected = [prep_dict_for_compare(serie) for serie in series_expected]
+        
+        # Check lengths are the same
+        self.assertEqual(len(series_result), len(series_expected))
+        
+        # Check content is the same
+        for idx in range(len(series_expected)):
+            with self.subTest(expected = series_expected[idx]):        
+                self.assertDictEqual(series_result[idx], series_expected[idx])
 
 if __name__ == '__main__':
     unittest.main()
---
+++ data/tests/utilities.py
@@ -0,0 +1,17 @@
+import ast
+import numpy
+import pytest as pt
+
+def prep_dict_for_compare(dict):
+    
+    # If type of dict is str, convert str representation of dict to dict
+    if isinstance(dict, str):
+        dict = ast.literal_eval(dict)
+        
+    # If dict values are floats, add acceptible relative tolerance of 1e-8
+    # See pytest.approx: https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-approx
+    if all([isinstance(v, numpy.float64) for v in dict.values()]):
+        dict = {i:pt.approx(x, rel=1e-8) for i, x in dict.items()}
+        
+    return dict
+    
