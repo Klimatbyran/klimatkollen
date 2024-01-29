ba2ececaa0c374423014c9f02f9d72bbf0aaaaf2
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2024-01-29 10:37:43 +0100

    Update test utilities function for float comparison tolerance

--- data/tests/utilities.py
+++ data/tests/utilities.py
@@ -10,8 +10,9 @@ def prep_dict_for_compare(dict):
         
     # If dict values are floats, add acceptible relative tolerance of 1e-8
     # See pytest.approx: https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-approx
-    if all([isinstance(v, float) for v in dict.values()]):
+    if all([isinstance(v, (float, numpy.float64, int)) for v in dict.values()]):
         dict = {i:pt.approx(x, rel=1e-8) for i, x in dict.items()}
+    
         
     return dict
     
