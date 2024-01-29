80a353c2207cf43a0ced6237505941eac0e4de65
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2024-01-29 10:53:22 +0100

    Add trend emission calculation test

--- data/tests/test_emission_calculations.py
+++ data/tests/test_emission_calculations.py
@@ -53,8 +53,28 @@ class TestEmissionCalculations(unittest.TestCase):
         
         # Check content of data series, per municipality
         for idx in range(len(series_expected)):
-            with self.subTest(expected = series_expected[idx]):        
+            with self.subTest(municipality = df_expected.iloc[idx]['Kommun']):        
                 self.assertDictEqual(series_result[idx], series_expected[idx])
+                
+    def test_calculate_trend(self):
+        path_input_df = 'tests/reference_dataframes/df_paris.xlsx'
+        path_expected_df = 'tests/reference_dataframes/df_trend.xlsx'
+        
+        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_result = calculate_trend(df_input)
+        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        
+        # Extract paris path data series and prepare for comparison
+        series_result = [prep_dict_for_compare(serie) for serie in df_result['trend']]
+        series_expected = [prep_dict_for_compare(serie) for serie in df_expected['trend']]
+    
+        # Check length of series
+        self.assertEqual(len(series_result), len(series_expected))
+        
+        # Check content of data series, per municipality
+        for idx in range(len(series_expected)):
+            with self.subTest(municipality = df_expected.iloc[idx]['Kommun']):  
+                self.assertEqual(series_result[idx], series_expected[idx])
 
 if __name__ == '__main__':
     unittest.main()
