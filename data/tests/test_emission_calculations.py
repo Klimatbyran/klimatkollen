270364584970873e1a5a629dfa02f14450e7cbff
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2024-02-12 14:11:07 +0100

    Add unit test, refactor test utilities and update test reference data frames with new budget

--- data/tests/test_emission_calculations.py
+++ data/tests/test_emission_calculations.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+import datetime
 import unittest
 import pandas as pd
-import numpy as np
 
 from issues.emissions.emission_data_calculations import get_n_prep_data_from_smhi, deduct_cement, calculate_municipality_budgets, calculate_paris_path, calculate_trend, calculate_change_percent, calculate_hit_net_zero, calculate_budget_runs_out
-from tests.utilities import prep_dict_for_compare, str_to_dict, prep_date_str_for_compare, str_to_numpy_array
+from tests.utilities import get_df_from_excel, prep_floats_for_compare, str_to_literal_structure, prep_date_str_for_compare, str_to_numpy_array
 
 class TestEmissionCalculations(unittest.TestCase):
     
@@ -12,27 +12,27 @@ class TestEmissionCalculations(unittest.TestCase):
         path_input_df = 'tests/reference_dataframes/df_municipalities.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_smhi.xlsx'
         
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_input = get_df_from_excel(path_input_df)
         df_result = get_n_prep_data_from_smhi(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         pd.testing.assert_frame_equal(df_result, df_expected, check_dtype=False)
 
     def test_deduct_cement(self):
         path_input_df = 'tests/reference_dataframes/df_smhi.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_cem.xlsx'
         
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_input = get_df_from_excel(path_input_df)
         df_result = deduct_cement(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         pd.testing.assert_frame_equal(df_result, df_expected, check_dtype=False)
         
     def test_calculate_municipality_budgets(self):
         path_input_df = 'tests/reference_dataframes/df_cem.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_budgeted.xlsx'
         
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_input = get_df_from_excel(path_input_df)
         df_result = calculate_municipality_budgets(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         pd.testing.assert_frame_equal(df_result, df_expected, check_dtype=False)
     
                 
@@ -40,13 +40,13 @@ class TestEmissionCalculations(unittest.TestCase):
         path_input_df = 'tests/reference_dataframes/df_budgeted.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_trend.xlsx'
         
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_input = get_df_from_excel(path_input_df)
         df_result = calculate_trend(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         
-        # Extract trend data dicts and prepare for comparison
-        dicts_result = [prep_dict_for_compare(serie) for serie in df_result['trend']]
-        dicts_expected = [prep_dict_for_compare(serie) for serie in df_expected['trend']]
+        # Extract trend data dicts and prepare floats for comparison (ignore round off differences due to floating point)
+        dicts_result = [prep_floats_for_compare(dict) for dict in df_result['trend']]
+        dicts_expected = [prep_floats_for_compare(dict) for dict in df_expected['trend']]
     
         # Check length of data dicts
         self.assertEqual(len(dicts_result), len(dicts_expected))
@@ -63,13 +63,13 @@ class TestEmissionCalculations(unittest.TestCase):
         path_input_df = 'tests/reference_dataframes/df_trend.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_paris.xlsx'
         
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
+        df_input = get_df_from_excel(path_input_df)
         df_result = calculate_paris_path(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         
-        # Extract paris path data dicts and prepare for comparison
-        dicts_result = [prep_dict_for_compare(serie) for serie in df_result['parisPath']]
-        dicts_expected = [prep_dict_for_compare(serie) for serie in df_expected['parisPath']]
+        # Extract paris path data dicts and prepare floats for comparison (ignore round off differences due to floating point)
+        dicts_result = [prep_floats_for_compare(dict) for dict in df_result['parisPath']]
+        dicts_expected = [prep_floats_for_compare(dict) for dict in df_expected['parisPath']]
     
         # Check length of data dicts
         self.assertEqual(len(dicts_result), len(dicts_expected))
@@ -84,12 +84,9 @@ class TestEmissionCalculations(unittest.TestCase):
         path_input_df = 'tests/reference_dataframes/df_paris.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_change_percent.xlsx'
         
-        # Get input df and convert parisPath data dicts from str to dict
-        df_input = pd.DataFrame(pd.read_excel(path_input_df)) 
-        df_input['parisPath'] = [str_to_dict(serie) for serie in df_input['parisPath']]
-        
+        df_input = get_df_from_excel(path_input_df) 
         df_result = calculate_change_percent(df_input)
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         
         pd.testing.assert_series_equal(df_result['emissionChangePercent'], df_expected['emissionChangePercent'])
         
@@ -97,17 +94,11 @@ class TestEmissionCalculations(unittest.TestCase):
         path_input_df = 'tests/reference_dataframes/df_change_percent.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_net_zero.xlsx'
         
-        # Get input df and convert trend data dicts from str to dict
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))  
-        df_input['trendCoefficients'] = [str_to_numpy_array(array) for array in df_input['trendCoefficients']]
-        df_input['trend'] = [str_to_dict(serie) for serie in df_input['trend']]
-        
+        df_input = get_df_from_excel(path_input_df)
         df_result = calculate_hit_net_zero(df_input)
-        
-        # Get expected df
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
     
-        # Extract hit net zero dates and prepare for comparison
+        # Extract hit net zero dates and prepare for comparison (format to ISO YYYY-MM-DD)
         df_result['hitNetZero'] = [prep_date_str_for_compare(date) for date in df_result['hitNetZero']]
         df_expected['hitNetZero'] = [prep_date_str_for_compare(date) for date in df_expected['hitNetZero']]
         
@@ -116,23 +107,67 @@ class TestEmissionCalculations(unittest.TestCase):
     def test_calculate_budget_runs_out(self):
         path_input_df = 'tests/reference_dataframes/df_net_zero.xlsx'
         path_expected_df = 'tests/reference_dataframes/df_budget_runs_out.xlsx'
-        
-        # Get input df and convert trend data dicts from str to dict        
-        df_input = pd.DataFrame(pd.read_excel(path_input_df))
-        df_input['trendCoefficients'] = [str_to_numpy_array(array) for array in df_input['trendCoefficients']] 
-        df_input['trend'] = [str_to_dict(serie) for serie in df_input['trend']]
-        
+            
+        df_input = get_df_from_excel(path_input_df)
         df_result = calculate_budget_runs_out(df_input)
-    
-        # Get expected df
-        df_expected = pd.DataFrame(pd.read_excel(path_expected_df))
+        df_expected = get_df_from_excel(path_expected_df)
         
-        # Extract budget runs out dates and prepare for comparison
+        # Extract budget runs out dates and prepare for comparison (format to ISO YYYY-MM-DD)
         df_result['budgetRunsOut'] = [prep_date_str_for_compare(date) for date in df_result['budgetRunsOut']]
         df_expected['budgetRunsOut'] = [prep_date_str_for_compare(date) for date in df_expected['budgetRunsOut']]
         
         pd.testing.assert_series_equal(df_result['budgetRunsOut'], df_expected['budgetRunsOut'])
+
+    def test_budget_runs_out_single_municipality_explicitly(self):
+        # Sample DataFrame for municipality 'Ale'
+        df_input = pd.DataFrame(
+            {
+                'Kommun': ['Ale'],
+                "trend": [
+                    {
+                        2021: 140535.25077554,
+                        2022: 142936.95388118387,
+                        2023: 143719.28805910517,
+                        2024: 144501.62223702623,
+                        2025: 145283.9564149473,
+                        2026: 146066.29059286858,
+                        2027: 146848.62477078964,
+                        2028: 147630.95894871093,
+                        2029: 148413.293126632,
+                        2030: 149195.6273045533,
+                        2031: 149977.96148247435,
+                        2032: 150760.2956603954,
+                        2033: 151542.6298383167,
+                        2034: 152324.96401623776,
+                        2035: 153107.29819415906,
+                        2036: 153889.63237208012,
+                        2037: 154671.96655000118,
+                        2038: 155454.30072792247,
+                        2039: 156236.63490584353,
+                        2040: 157018.96908376482,
+                        2041: 157801.30326168588,
+                        2042: 158583.63743960718,
+                        2043: 159365.97161752824,
+                        2044: 160148.3057954493,
+                        2045: 160930.6399733706,
+                        2046: 161712.97415129165,
+                        2047: 162495.30832921294,
+                        2048: 163277.642507134,
+                        2049: 164059.97668505507,
+                        2050: 164842.31086297636,
+                    }
+                ],
+                "trendCoefficients": [[7.82334178e02, -1.43894275e06]],
+                "Budget": [286595.380915185],
+            }
+        )
         
+        df_result = calculate_budget_runs_out(df_input)
+        result_date = df_result.iloc[0]['budgetRunsOut']
+        
+        expected_date = datetime.date(2025, 12, 22)
+
+        self.assertEqual(result_date, expected_date)
 
 if __name__ == '__main__':
     unittest.main()
--- data/tests/utilities.py
+++ data/tests/utilities.py
@@ -2,6 +2,43 @@ import ast
 from datetime import datetime
 import numpy as np
 import pytest as pt
+import pandas as pd
+
+def get_df_from_excel(path_to_df):
+    # Read df from excel
+    df = pd.DataFrame(pd.read_excel(path_to_df))
+    
+    # Checks columns which need to be parsed from string represetations
+    if 'trend' in df.columns:
+        df['trend']  = [str_to_literal_structure(dict) for dict in df['trend']]
+    if 'trendCoefficients' in df.columns:
+        df['trendCoefficients'] = [str_to_numpy_array(array) for array in df['trendCoefficients']] 
+    if 'parisPath' in df.columns:
+        df['parisPath']  = [str_to_literal_structure(dict) for dict in df['parisPath']]
+        
+    return df
+
+def str_to_literal_structure(param):
+    # If string represetation, convert to literal structure
+    if isinstance(param, str):
+        param = ast.literal_eval(param)
+        
+    return param
+
+def str_to_numpy_array(array):
+    # Convert string representation of numpy array to to numpy array
+    if isinstance(array, str):
+        array = np.fromstring(array[1:-1], dtype=np.float64, sep=' ')
+    
+    return array
+
+def prep_floats_for_compare(dict):
+    # If dict values are floats, add acceptible relative tolerance of 1e-8
+    # See pytest.approx: https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-approx
+    if any([isinstance(v, (float, np.floating)) for v in dict.values()]):
+        dict = {i:pt.approx(x, rel=1e-8) for i, x in dict.items()}
+        
+    return dict
 
 def prep_date_str_for_compare(date_str):
     if date_str != 'Aldrig':
@@ -9,28 +46,4 @@ def prep_date_str_for_compare(date_str):
         date_str = datetime.strftime(date_str, "%Y-%m-%d")
     
     return date_str
-
-def str_to_dict(dict):
-    # If dict is a string represetation, convert to dict
-    if isinstance(dict, str):
-        dict = ast.literal_eval(dict)
-        
-    return dict
-
-def str_to_numpy_array(array):
-    # Convert string representation of numpy array to to numpy array
-    array = np.fromstring(array[1:-1], dtype=np.float64, sep=' ')
-    
-    return array
-
-def prep_dict_for_compare(dict):
-    # Convert str to dict, if needed
-    dict = str_to_dict(dict)
-    
-    # If dict values are floats, add acceptible relative tolerance of 1e-8
-    # See pytest.approx: https://docs.pytest.org/en/7.1.x/reference/reference.html#pytest-approx
-    if any([isinstance(v, (float, np.floating)) for v in dict.values()]):
-        dict = {i:pt.approx(x, rel=1e-8) for i, x in dict.items()}
-        
-    return dict
     
