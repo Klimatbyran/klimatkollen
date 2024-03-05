1f324e190c9c9d079ae452382e92332745830e5d
Elvira <elvbom@users.noreply.github.com>
2024-03-05 21:53:04 +0100

    add regionalview

---
+++ components/RegionalView.tsx
@@ -0,0 +1,179 @@
+import dynamic from 'next/dynamic'
+import styled from 'styled-components'
+import { NextRouter } from 'next/router'
+import ComparisonTable from '../components/ComparisonTable'
+import MapLabels from '../components/Map/MapLabels'
+import ListIcon from '../public/icons/list.svg'
+import MapIcon from '../public/icons/map.svg'
+import ToggleButton from '../components/ToggleButton'
+import RadioButtonMenu from './RadioButtonMenu'
+import DropDown from '../components/DropDown'
+import { H5Regular, Paragraph } from './Typography'
+import { devices } from '../utils/devices'
+import {
+  datasetDescriptions,
+  currentData,
+  defaultDataView,
+  secondaryDataView,
+} from '../utils/datasetDescriptions'
+import { Municipality, SelectedData } from '../utils/types'
+import { normalizeString } from '../utils/shared'
+import { listColumns, rankData } from '../utils/createMunicipalityList'
+
+const Map = dynamic(() => import('../components/Map/Map'))
+
+const InfoText = styled.div`
+  padding: 0 16px;
+`
+
+const ParagraphSource = styled(Paragraph)`
+  font-size: 13px;
+  color: ${({ theme }) => theme.grey};
+`
+
+const InfoContainer = styled.div`
+  width: 100%;
+  position: relative;
+  background: ${({ theme }) => theme.lightBlack};
+  border-radius: 8px;
+  margin-bottom: 32px;
+  z-index: 15;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const FloatingH5 = styled(H5Regular)`
+  position: absolute;
+  margin: 60px 0 0 16px;
+  z-index: 200;
+
+  @media only screen and (${devices.mobile}) {
+    margin: 55px 0 0 16px;
+  }
+`
+
+const ComparisonContainer = styled.div<{ $dataView: string }>`
+  position: relative;
+  overflow-y: scroll;
+  z-index: 100;
+  // TODO: Hardcoding this is not good.
+  height: 400px;
+  border-radius: 8px;
+  display: flex;
+  margin-top: ${({ $dataView }) => ($dataView === secondaryDataView ? '64px' : '0')};
+
+  @media only screen and (${devices.tablet}) {
+    height: 500px;
+  }
+
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  ::-webkit-scrollbar {
+    /* Chrome, Safari and Opera */
+    display: none;
+  }
+`
+
+type RegionalViewProps = {
+  municipalities: Array<Municipality>
+  selectedDataset: SelectedData
+  setSelectedDataset: (newData: SelectedData) => void
+  selectedDataView: string
+  setSelectedDataView: (newData: string) => void
+  router: NextRouter
+}
+
+function RegionalView({
+  municipalities,
+  selectedDataset,
+  setSelectedDataset,
+  selectedDataView,
+  setSelectedDataView,
+  router,
+}: RegionalViewProps) {
+  const handleDataChange = (newData: SelectedData) => {
+    const newDataString = newData as string
+    setSelectedDataset(newDataString)
+    const normalizedDataset = normalizeString(newDataString)
+    router.push(`/${normalizedDataset}/${selectedDataView}`, undefined, {
+      shallow: true,
+      scroll: false,
+    })
+  }
+
+  const municipalityNames = municipalities.map((item) => item.Name) // get all municipality names for drop down
+  const municipalityData = currentData(municipalities, selectedDataset) // get all municipality names and data points for map and list
+  const datasetDescription = datasetDescriptions[selectedDataset] // get description of selected dataset
+
+  const handleToggleView = () => {
+    const newDataView = selectedDataView === defaultDataView ? secondaryDataView : defaultDataView
+    setSelectedDataView(newDataView)
+    router.replace(
+      `/${normalizeString(selectedDataset as string)}/${newDataView}`,
+      undefined,
+      {
+        shallow: true,
+        scroll: false,
+      },
+    )
+  }
+
+  const cols = listColumns(selectedDataset, datasetDescription)
+  const rankedData = rankData(municipalities, selectedDataset)
+
+  const isDefaultDataView = selectedDataView === defaultDataView
+
+  return (
+    <>
+      <RadioButtonMenu
+        selectedData={selectedDataset}
+        handleDataChange={handleDataChange}
+      />
+      <InfoContainer>
+        <TitleContainer>
+          <FloatingH5>{datasetDescription.title}</FloatingH5>
+          <ToggleButton
+            handleClick={handleToggleView}
+            text={isDefaultDataView ? 'Listvy' : 'Kartvy'}
+            icon={isDefaultDataView ? <ListIcon /> : <MapIcon />}
+          />
+        </TitleContainer>
+        <ComparisonContainer $dataView={selectedDataView.toString()}>
+          {isDefaultDataView && (
+          <>
+            <MapLabels
+              labels={datasetDescription.labels}
+              rotations={datasetDescription.labelRotateUp}
+            />
+            <Map
+              data={municipalityData}
+              boundaries={datasetDescription.boundaries}
+            />
+          </>
+          )}
+          {selectedDataView === secondaryDataView && (
+          <ComparisonTable data={rankedData[selectedDataset]} columns={cols} />
+          )}
+        </ComparisonContainer>
+        <InfoText>
+          <Paragraph>{datasetDescription.body}</Paragraph>
+          <ParagraphSource>{datasetDescription.source}</ParagraphSource>
+        </InfoText>
+      </InfoContainer>
+      <DropDown
+        className="startpage"
+        municipalitiesName={municipalityNames}
+        placeholder="Hur går det i din kommun?"
+      />
+    </>
+  )
+}
+
+export default RegionalView
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,104 +1,31 @@
 import { GetServerSideProps } from 'next'
-import dynamic from 'next/dynamic'
 import { ReactElement, useEffect, useState } from 'react'
 import styled from 'styled-components'
-
 import { useRouter } from 'next/router'
-import DropDown from '../components/DropDown'
 import MetaTags from '../components/MetaTags'
-import { H2Regular, H5Regular, Paragraph } from '../components/Typography'
+import { H2Regular } from '../components/Typography'
 import { Municipality, SelectedData } from '../utils/types'
 import PageWrapper from '../components/PageWrapper'
-import { devices } from '../utils/devices'
 import Layout from '../components/Layout'
 import Footer from '../components/Footer/Footer'
-import ComparisonTable from '../components/ComparisonTable'
-import MapLabels from '../components/Map/MapLabels'
-import ListIcon from '../public/icons/list.svg'
-import MapIcon from '../public/icons/map.svg'
-import ToggleButton from '../components/ToggleButton'
 import {
   defaultDataset,
-  datasetDescriptions,
-  currentData,
   defaultDataView,
-  secondaryDataView,
 } from '../utils/datasetDescriptions'
-import RadioButtonMenu from '../components/RadioButtonMenu'
-import { listColumns, rankData } from '../utils/createMunicipalityList'
 import {
   isValidDataView,
   isValidDataset,
   normalizeString,
   validDatasetsMap,
 } from '../utils/shared'
-
-const Map = dynamic(() => import('../components/Map/Map'))
+import PillSwitch from '../components/PillSwitch'
+import RegionalView from '../components/RegionalView'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   align-items: center;
-`
-
-const InfoText = styled.div`
-  padding: 0 16px;
-`
-
-const ParagraphSource = styled(Paragraph)`
-  font-size: 13px;
-  color: ${({ theme }) => theme.grey};
-`
-
-const InfoContainer = styled.div`
-  width: 100%;
-  position: relative;
-  background: ${({ theme }) => theme.lightBlack};
-  border-radius: 8px;
-  margin-bottom: 32px;
-  z-index: 15;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
-
-const TitleContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
-const FloatingH5 = styled(H5Regular)`
-  position: absolute;
-  margin: 60px 0 0 16px;
-  z-index: 200;
-
-  @media only screen and (${devices.mobile}) {
-    margin: 55px 0 0 16px;
-  }
-`
-
-const ComparisonContainer = styled.div<{ $dataView: string }>`
-  position: relative;
-  overflow-y: scroll;
-  z-index: 100;
-  // TODO: Hardcoding this is not good.
-  height: 400px;
-  border-radius: 8px;
-  display: flex;
-  margin-top: ${({ $dataView }) => ($dataView === secondaryDataView ? '64px' : '0')};
-
-  @media only screen and (${devices.tablet}) {
-    height: 500px;
-  }
-
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-  ::-webkit-scrollbar {
-    /* Chrome, Safari and Opera */
-    display: none;
-  }
 `
 
 type PropsType = {
@@ -116,6 +43,8 @@ function StartPage({ municipalities }: PropsType) {
   const [selectedDataset, setSelectedDataset] = useState<SelectedData>(defaultDataset)
   const [selectedDataView, setSelectedDataView] = useState(normalizedDataView)
 
+  const [showRegionalEmissionData, setShowRegionalEmissionData] = useState(true)
+
   useEffect(() => {
     if (normalizedRouteDataset && isValidDataset(normalizedRouteDataset)) {
       setSelectedDataset(validDatasetsMap[normalizedRouteDataset])
@@ -128,38 +57,6 @@ function StartPage({ municipalities }: PropsType) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const handleDataChange = (newData: SelectedData) => {
-    const newDataString = newData as string
-    setSelectedDataset(newDataString)
-    const normalizedDataset = normalizeString(newDataString)
-    router.push(`/${normalizedDataset}/${selectedDataView}`, undefined, {
-      shallow: true,
-      scroll: false,
-    })
-  }
-
-  const municipalityNames = municipalities.map((item) => item.Name) // get all municipality names for drop down
-  const municipalityData = currentData(municipalities, selectedDataset) // get all municipality names and data points for map and list
-  const datasetDescription = datasetDescriptions[selectedDataset] // get description of selected dataset
-
-  const handleToggle = () => {
-    const newDataView = selectedDataView === defaultDataView ? secondaryDataView : defaultDataView
-    setSelectedDataView(newDataView)
-    router.replace(
-      `/${normalizeString(selectedDataset as string)}/${newDataView}`,
-      undefined,
-      {
-        shallow: true,
-        scroll: false,
-      },
-    )
-  }
-
-  const cols = listColumns(selectedDataset, datasetDescription)
-  const rankedData = rankData(municipalities, selectedDataset) // fixme hur byter jag ut denna till municipalityData?
-
-  const isDefaultDataView = selectedDataView === defaultDataView
-
   return (
     <>
       <MetaTags
@@ -168,47 +65,20 @@ function StartPage({ municipalities }: PropsType) {
       />
       <PageWrapper backgroundColor="black">
         <Container>
+          <PillSwitch onToggle={setShowRegionalEmissionData} />
           <H2Regular>Hur går det med?</H2Regular>
-          <RadioButtonMenu
-            selectedData={selectedDataset}
-            handleDataChange={handleDataChange}
-          />
-          <InfoContainer>
-            <TitleContainer>
-              <FloatingH5>{datasetDescription.title}</FloatingH5>
-              <ToggleButton
-                handleClick={handleToggle}
-                text={isDefaultDataView ? 'Listvy' : 'Kartvy'}
-                icon={isDefaultDataView ? <ListIcon /> : <MapIcon />}
+          {showRegionalEmissionData
+            ? (
+              <RegionalView
+                municipalities={municipalities}
+                selectedDataset={selectedDataset}
+                setSelectedDataset={setSelectedDataset}
+                selectedDataView={selectedDataView}
+                setSelectedDataView={setSelectedDataView}
+                router={router}
               />
-            </TitleContainer>
-            <ComparisonContainer $dataView={selectedDataView.toString()}>
-              {isDefaultDataView && (
-                <>
-                  <MapLabels
-                    labels={datasetDescription.labels}
-                    rotations={datasetDescription.labelRotateUp}
-                  />
-                  <Map
-                    data={municipalityData}
-                    boundaries={datasetDescription.boundaries}
-                  />
-                </>
-              )}
-              {selectedDataView === secondaryDataView && (
-                <ComparisonTable data={rankedData[selectedDataset]} columns={cols} />
-              )}
-            </ComparisonContainer>
-            <InfoText>
-              <Paragraph>{datasetDescription.body}</Paragraph>
-              <ParagraphSource>{datasetDescription.source}</ParagraphSource>
-            </InfoText>
-          </InfoContainer>
-          <DropDown
-            className="startpage"
-            municipalitiesName={municipalityNames}
-            placeholder="Hur går det i din kommun?"
-          />
+            )
+            : <></>}
         </Container>
       </PageWrapper>
     </>
