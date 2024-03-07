c36fa81e5fa039c240c06669092fbc3819a2de3b
Simon <contact@smn.codes>
2024-03-07 19:36:36 +0100

    Create a separate title for the municipality search box

--- pages/index.tsx
+++ pages/index.tsx
@@ -101,6 +101,10 @@ const ComparisonContainer = styled.div<{ $dataView: string }>`
   }
 `
 
+const DropDownTitle = styled(H2Regular)`
+  margin-bottom: 20px;
+`
+
 type PropsType = {
   municipalities: Array<Municipality>
 }
@@ -204,10 +208,11 @@ function StartPage({ municipalities }: PropsType) {
               <ParagraphSource>{datasetDescription.source}</ParagraphSource>
             </InfoText>
           </InfoContainer>
+          <DropDownTitle>Hur går det i din kommun?</DropDownTitle>
           <DropDown
             className="startpage"
             municipalitiesName={municipalityNames}
-            placeholder="Hur går det i din kommun?"
+            placeholder="Sök efter din kommun"
           />
         </Container>
       </PageWrapper>
