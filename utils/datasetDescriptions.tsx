275ac19e72bd8cc3d359390a341013af9ee2e874
Charlotta Jaunviksna <charlotta.jaunviksna@precisit.com>
2024-03-07 10:35:15 +0100

    Shorten CO2 budget title in map

--- utils/datasetDescriptions.tsx
+++ utils/datasetDescriptions.tsx
@@ -200,7 +200,7 @@ export const datasetDescriptions: DatasetDescriptions = {
   },
 
   Koldioxidbudgetarna: {
-    title: 'Koldioxidbudget slut om',
+    title: 'Budget slut om',
     body: 'Datum då kommunens koldioxidbudget tar slut om utsläppen fortsätter enligt nuvarande trend. Några kommuner kommer att hålla budgeten om trenden står sig.',
     source: (
       <>
