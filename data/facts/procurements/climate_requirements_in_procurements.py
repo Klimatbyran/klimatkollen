45dffa08d2b37fbf47cd0b48d8addf6f3838ac80
Elvira <elvbom@users.noreply.github.com>
2024-03-18 13:15:41 +0100

    add test and correct procurement calculation

--- data/facts/procurements/climate_requirements_in_procurements.py
+++ data/facts/procurements/climate_requirements_in_procurements.py
@@ -7,26 +7,35 @@ from helpers import clean_kommun
 PATH_NUR_DATA = 'facts/procurements/NUE2022_DATA_2023-12-20.xlsx'
 PATH_GREENPEACE_DATA = 'facts/procurements/Klimatkrav offentlig upphandling kartläggning.xlsx'
 
+def get_greenpeace_data():
+    df_greenpeace = pd.read_excel(PATH_GREENPEACE_DATA)
+    df_greenpeace['hasLink'] = df_greenpeace['Länk\nKlimat\nkrav']
+    df_greenpeace['hasLink'] = df_greenpeace['hasLink'].apply(lambda x: 1 if pd.notnull(x) else 0)
+    return df_greenpeace[['Kommun', 'hasLink']]
+
+def get_nur_data():
+    df_nur = pd.read_excel(PATH_NUR_DATA)
+    df_nur['Kommun'] = df_nur['ORG_NAMN'].apply(clean_kommun)
+    return df_nur[['Kommun', 'BINÄRT_UTFALL']]
+
+def calculate_procurement_score(df_greenpeace, df_nur):
+    df_merge = pd.merge(df_greenpeace, df_nur, on='Kommun', how='left')
+    df_merge['procurementScore'] = df_merge.apply(
+        lambda row: 2 if row['hasLink'] > 0 else 1 if row['BINÄRT_UTFALL'] > 0 else 0, axis=1
+        )
+    df_fill = df_merge.fillna(0) 
+    return df_fill[['Kommun', 'procurementScore']]
+
 def get_procurement_data():
     """
     Retrieves climate requirements in procurement data.
     """
 
-    # Read Greenpeace data
-    df_greenpeace = pd.read_excel(PATH_GREENPEACE_DATA)
-    df_greenpeace['hasLink'] = df_greenpeace['Länk\nKlimat\nkrav']
-    df_greenpeace['hasLink'] = df_greenpeace['hasLink'].apply(lambda x: 1 if pd.notnull(x) else 0)
-    df_greenpeace_filter = df_greenpeace[['Kommun', 'hasLink']]
+    # Read and clean Greenpeace data
+    df_greenpeace = get_greenpeace_data()
 
-    # Read NUR data
-    df_NUR = pd.read_excel(PATH_NUR_DATA)
-    df_NUR['Kommun'] = df_NUR['ORG_NAMN'].apply(clean_kommun)
-    df_NUR_filter = df_NUR[['Kommun', 'BINÄRT_UTFALL']].fillna(0)
+    # Read and clean NUR data
+    df_nur = get_nur_data()
 
-    # Merge the two dataframes
-    df_merged = pd.merge(df_greenpeace_filter, df_NUR_filter, on='Kommun', how='left')
-
-    # Calculate procurement score
-    df_merged['procurementScore'] = df_merged.apply(lambda row: 2 if row['BINÄRT_UTFALL'] > 0 else row['hasLink'], axis=1)
-
-    return df_merged
+    # Return df with calculated procurement scores
+    return calculate_procurement_score(df_greenpeace, df_nur)
