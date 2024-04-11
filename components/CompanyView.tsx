9e49dd7b02820e179cc40b85ff0a8ce475fda4a2
Elvira <elvbom@users.noreply.github.com>
2024-04-11 16:24:59 +0200

    add todo

--- components/CompanyView.tsx
+++ components/CompanyView.tsx
@@ -92,4 +92,13 @@ function CompanyView({
   )
 }
 
+/**
+ * TODO
+ * [] add tooltip on hover
+ * [] routing
+ * [] data from API not excel
+ * [] styling
+ * [] texts
+ * /
+
 export default CompanyView
