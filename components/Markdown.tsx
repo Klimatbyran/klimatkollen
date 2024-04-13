16e5c6b5642966c90cbcccf69fec9ab7e6355af4
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-04-13 15:13:38 +0200

    Improve styles for Markdown

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,5 +1,7 @@
 import ReactMarkdown, { Components, Options } from 'react-markdown'
 import rehypeExternalLinks from 'rehype-external-links'
+import styled from 'styled-components'
+
 import {
   H1, H2, H3, Paragraph, ParagraphBold, ParagraphItalic,
 } from './Typography'
@@ -7,9 +9,17 @@ import {
 const optionsForRehypeExternalLinks = { rel: ['noreferrer'], target: '_blank' }
 
 const defaultComponents: Partial<Components> = {
-  p: Paragraph,
-  strong: ParagraphBold,
-  em: ParagraphItalic,
+  p: styled(Paragraph)`
+    &:first-child {
+      margin-top: 0;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  `,
+  // Workaround to inherit styles and just change the HTML element
+  strong: styled(ParagraphBold).attrs({ as: 'strong' })``,
+  em: styled(ParagraphItalic).attrs({ as: 'em' })``,
   h1: H1,
   h2: H2,
   h3: H3,
