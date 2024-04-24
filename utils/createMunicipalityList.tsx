e039129b5af3590a06370f06f8da613d0134fc4f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-04-24 12:24:58 +0200

    Fix sorting for klimatplanerna

--- utils/createMunicipalityList.tsx
+++ utils/createMunicipalityList.tsx
@@ -17,13 +17,6 @@ type RowData = {
   secondaryDataPoint?: string
 }
 
-export const calculateClimatePlanRankings = (
-  data: Array<{ name: string; dataPoint: string | number | Date; formattedDataPoint: string }>,
-) => data.map((item) => ({
-  ...item,
-  index: item.dataPoint === climatePlanMissing ? 1 : -1,
-}))
-
 const getCustomSortFn = ({
   stringsOnTop = false, sortAscending = false,
 }: {
@@ -44,6 +37,50 @@ const getCustomSortFn = ({
   return sortAscending ? a - b : b - a
 }
 
+const sortClimatePlans = (a: number | typeof climatePlanMissing, b: number | typeof climatePlanMissing) => {
+  // If both A and B are missing climate plans, then return 0
+  if (a === climatePlanMissing && b === climatePlanMissing) {
+    return 0
+  }
+
+  // If A is missing a climate plan, but B has one, then A should be after B, and we should return 1
+  if (a === climatePlanMissing && !Number.isNaN(b)) {
+    return 1
+  }
+
+  // If A has a climate plan, but B is missing one, then A should be before B, and we should return -1
+  if (!Number.isNaN(a) && b === climatePlanMissing) {
+    return -1
+  }
+
+  // If both A and B have climate plans, then we should return A-B and compare the years when they were adopted
+  return (b as number) - (a as number)
+}
+
+const climatePlansSortingFn = (rowA: Row<RowData>, rowB: Row<RowData>) => {
+  const aVal = rowA.original.secondaryDataPoint!
+  const bVal = rowB.original.secondaryDataPoint!
+
+  const a = aVal === climatePlanMissing ? aVal : Number(aVal)
+  const b = bVal === climatePlanMissing ? bVal : Number(bVal)
+
+  return sortClimatePlans(a, b)
+}
+
+export const calculateClimatePlanRankings = (
+  data: Omit<RowData, 'index'>[],
+) => data.map((item, i) => ({ ...item, index: i + 1 })).sort((rowA, rowB) => {
+  const aVal = rowA.secondaryDataPoint!
+  const bVal = rowB.secondaryDataPoint!
+
+  const a = aVal === climatePlanMissing ? aVal : Number(aVal)
+  const b = bVal === climatePlanMissing ? bVal : Number(bVal)
+
+  // NOTE: Sorting seems to work, just need cleanup refactor now
+
+  return sortClimatePlans(a, b)
+})
+
 export const calculateRankings = (
   data: Array<{ name: string; dataPoint: number; formattedDataPoint: string }>,
   sortAscending: boolean,
@@ -57,6 +94,11 @@ export const calculateRankings = (
     index: index + 1,
   }))
 }
+
+// TODO: for dataset koldioxidbudgetarna Maybe update title: "Budget slut om" since it is unclear for the table header
+// TODO: for dataset klimatplanerna, the first column "har plan" does not give correct sorting results. We need a custom sort there.
+// TODO: for all datasets that customize the first column to use string data, we need to add a custom sorting function.
+// TODO: for dataset upphandlingarna, when "underlag" column has value "saknas", maybe show it gray color instead of orange since it is missing.
 
 export const rankData = (municipalities: Municipality[], selectedData: DatasetKey, locale: string, t: TFunction) => {
   const { dataDescriptions } = getDataDescriptions(locale as string, t)
@@ -113,8 +155,9 @@ export const listColumns = (
     return t('startPage:ranking')
   }
 
-  const firstColumnClimatePlans = (index: number, dataPoint: string) => (index === -1
-    ? (
+  const firstColumnClimatePlans = (dataPoint: string) => (dataPoint === climatePlanMissing
+    ? t('common:no')
+    : (
       <a
         href={dataPoint.toString()}
         onClick={(e) => e.stopPropagation()}
@@ -125,14 +168,13 @@ export const listColumns = (
         {t('common:yes')}
       </a>
     )
-    : t('common:no')
   )
 
   const getFirstColumnCell = (props: CellContext<RowData, unknown>) => {
     const { index, dataPoint } = props.row.original
 
     if (isClimatePlan) {
-      return firstColumnClimatePlans(index, dataPoint as string)
+      return firstColumnClimatePlans(dataPoint as string)
     }
 
     if (isProcurement) {
@@ -172,73 +214,17 @@ export const listColumns = (
     return formattedDataPoint
   }
 
+  // TODO: Move this config on to the dataset definitions instead
   const getSortingFn = (datasetKey: DatasetKey) => {
+    if (datasetKey === 'klimatplanerna') {
+      return climatePlansSortingFn
+    }
+
     let customSort: ReturnType<typeof getCustomSortFn> | undefined
     // TODO: Get the params passed to getCustomSortFn() from config, rather than duplicating and hard coding sortAscending and stringsOnTop
 
     if (datasetKey === 'koldioxidbudgetarna') {
       customSort = getCustomSortFn({ stringsOnTop: true })
-    } else if (datasetKey === 'klimatplanerna') {
-      // TODO: sorting for klimatplanerna is still broken for some reason. However, koldioxidbudgetarna and laddarna now works.
-      // customSort = getCustomSortFn({ sortAscending: false })
-      // customSort = getCustomSortFn({ stringsOnTop: true, sortAscending: false })
-      return (rowA: Row<RowData>, rowB: Row<RowData>) => {
-        // const a = rowA.original.dataPoint === climatePlanMissing ? rowA.original.dataPoint : rowA.original.secondaryDataPoint!
-        // const b = rowB.original.dataPoint === climatePlanMissing ? rowB.original.dataPoint : rowB.original.secondaryDataPoint!
-        // const a = rowA.original.dataPoint === climatePlanMissing ? rowA.original.dataPoint : rowA.original.secondaryDataPoint!
-        // const b = rowB.original.dataPoint === climatePlanMissing ? rowB.original.dataPoint : rowB.original.secondaryDataPoint!
-
-        const aVal = rowA.original.secondaryDataPoint!
-        const bVal = rowB.original.secondaryDataPoint!
-
-        const a = aVal === climatePlanMissing ? aVal : Number(aVal)
-        const b = bVal === climatePlanMissing ? bVal : Number(bVal)
-
-        // RETURN VALUES
-        // A negative value indicates that a should come before b.
-        // A positive value indicates that a should come after b.
-        // Zero or NaN indicates that a and b are considered equal.
-
-        // If both A and B are missing climate plans, then return 0
-        if (a === climatePlanMissing && b === climatePlanMissing) {
-          return 0
-        }
-
-        // If A is missing a climate plan, but B has one, then A should be after B, and we should return 1
-        if (a === climatePlanMissing && !Number.isNaN(b)) {
-          return 1
-        }
-
-        // If A has a climate plan, but B is missing one, then A should be before B, and we should return -1
-        if (!Number.isNaN(a) && b === climatePlanMissing) {
-          return -1
-        }
-
-        // If both A and B have climate plans, then we should return A-B and compare the years when they were adopted
-        return (a as number) - (b as number)
-
-        // return customSort!(a, b)
-
-        // const a = rowA.original.dataPoint === climatePlanMissing ? 1 : -1
-        // const b = rowB.original.dataPoint === climatePlanMissing ? 1 : -1
-
-        // IDEA: maybe compare against another datapoint
-        // const a = rowA.original.dataPoint === climatePlanMissing ? -1 : Number(rowA.original.secondaryDataPoint)
-        // const b = rowB.original.dataPoint === climatePlanMissing ? -1 : Number(rowB.original.secondaryDataPoint)
-
-        // maybe assign 0 to missing plans
-        // and then just subtract to get the diff and sort order
-
-        // if (a === climatePlanMissing && b === climatePlanMissing) {
-        //   return 0
-        // }
-
-        // if (a === climatePlanMissing || b === climatePlanMissing) {
-        //   return -1
-        // }
-
-      // return a - b
-      }
     } else if (datasetKey === 'laddarna') {
       customSort = getCustomSortFn({ sortAscending: true })
     }
