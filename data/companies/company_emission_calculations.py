6e7cf2ae362a577a38b06956ed737f469f7df615
Elvira <elvbom@users.noreply.github.com>
2024-05-07 14:14:56 +0200

    Feat/garbo updated company data (#502)
    * updated company data
    
    * rm leading and trailing spaces
    
    * Fix formatting for emission scopes that are not reported
    
    ---------
    
    Co-authored-by: Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>

--- data/companies/company_emission_calculations.py
+++ data/companies/company_emission_calculations.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 # Read the Excel file into a DataFrame
-df = pd.read_excel("mvp1.xlsx")
+df = pd.read_excel("companyData.xlsx", sheet_name="SiteData")
 
 # Select the desired columns
 selected_columns = ['Company', 'URL 2023', 'Scope 1+2', 'Scope 3 (total)', 'Alex kommentar']
 selected_df = df[selected_columns]
+
+# Strip leading and trailing whitespaces from all string columns
+selected_df = selected_df.map(lambda x: x.strip() if isinstance(x, str) else x)
 
 # Rename the columns for clarity
 selected_df.columns = ['Company', 'URL', 'Scope1n2', 'Scope3', 'Comment']
--- utils/createCompanyList.tsx
+++ utils/createCompanyList.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import { ColumnDef } from '@tanstack/react-table'
+import { TFunction } from 'i18next'
+
 import { Company } from './types'
 
 // IDEA: do something similar for the regional view to distinguish between actual important data (orange), and when something is missing (gray)
@@ -10,60 +12,64 @@ const ScopeColumn = styled.span<{ isMissing: boolean }>`
 
 const formatter = new Intl.NumberFormat('sv-SV', { maximumFractionDigits: 0 })
 
-export const companyColumns = (): ColumnDef<Company>[] => [
-  {
-    header: 'Företag',
-    cell: (row) => {
-      const company = row.cell.row.original
-      return company.Url ? (
-        <a
-          href={company.Url}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ cursor: 'pointer' }}
-        >
-          {company.Name}
-        </a>
-      ) : (
-        <span>{company.Name}</span>
-      )
-    },
-    accessorKey: 'Name',
-  },
-  {
-    header: 'Egna utsläpp (tCO₂e)',
-    cell: (row) => {
-      const scope1n2Emissions = row.cell.row.original.Emissions.Scope1n2
+export const companyColumns = (t: TFunction): ColumnDef<Company>[] => {
+  const notReported = t('common:notReported')
 
-      // console.log({ row, Emissions: row.cell.row.original.Emissions })
-      // NOTE: The type does not match the actual values here.
-      // TS thinks scope1n2Emissions has the type `CompanyScope`, but according to the logging above,
-      // it is in fact just a number or null.
-      const scope1n2String = Number.isFinite(scope1n2Emissions) ? formatter.format(scope1n2Emissions as unknown as number) : 'Ej rapporterat'
-      return (
-        <ScopeColumn isMissing={!scope1n2Emissions}>
-          {scope1n2String}
-        </ScopeColumn>
-      )
+  return [
+    {
+      header: 'Företag',
+      cell: (row) => {
+        const company = row.cell.row.original
+        return company.Url ? (
+          <a
+            href={company.Url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ cursor: 'pointer' }}
+          >
+            {company.Name}
+          </a>
+        ) : (
+          <span>{company.Name}</span>
+        )
+      },
+      accessorKey: 'Name',
     },
-    accessorKey: 'Emissions.Scope1n2',
-  },
-  {
-    header: () => 'Utsläpp i värdekedjan (tCO₂e)',
-    cell: (row) => {
-      const scope3Emissions = row.cell.row.original.Emissions.Scope3
+    {
+      header: 'Egna utsläpp (tCO₂e)',
+      cell: (row) => {
+        const scope1n2Emissions = row.cell.row.original.Emissions.Scope1n2
 
-      // console.log({ row, Emissions: row.cell.row.original.Emissions })
-      // NOTE: The type does not match the actual values here.
-      // TS thinks scope3Emissions has the type `CompanyScope`, but according to the logging above,
-      // it is in fact just a number or null.
-      const scope3String = Number.isFinite(scope3Emissions) ? formatter.format(scope3Emissions as unknown as number) : 'Ej rapporterat'
-      return (
-        <ScopeColumn isMissing={!scope3Emissions}>
-          {scope3String}
-        </ScopeColumn>
-      )
+        // console.log({ row, Emissions: row.cell.row.original.Emissions })
+        // NOTE: The type does not match the actual values here.
+        // TS thinks scope1n2Emissions has the type `CompanyScope`, but according to the logging above,
+        // it is in fact just a number or null.
+        const scope1n2String = Number.isFinite(scope1n2Emissions) ? formatter.format(scope1n2Emissions as unknown as number) : notReported
+        return (
+          <ScopeColumn isMissing={scope1n2String === notReported}>
+            {scope1n2String}
+          </ScopeColumn>
+        )
+      },
+      accessorKey: 'Emissions.Scope1n2',
     },
-    accessorKey: 'Emissions.Scope3',
-  },
-]
+    {
+      header: () => 'Utsläpp i värdekedjan (tCO₂e)',
+      cell: (row) => {
+        const scope3Emissions = row.cell.row.original.Emissions.Scope3
+
+        // console.log({ row, Emissions: row.cell.row.original.Emissions })
+        // NOTE: The type does not match the actual values here.
+        // TS thinks scope3Emissions has the type `CompanyScope`, but according to the logging above,
+        // it is in fact just a number or null.
+        const scope3String = Number.isFinite(scope3Emissions) ? formatter.format(scope3Emissions as unknown as number) : notReported
+        return (
+          <ScopeColumn isMissing={scope3String === notReported}>
+            {scope3String}
+          </ScopeColumn>
+        )
+      },
+      accessorKey: 'Emissions.Scope3',
+    },
+  ]
+}
