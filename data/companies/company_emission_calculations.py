43ecf7f3e4f8abd42c74ac036c9b8c50b8af9128
Elvira <elvbom@users.noreply.github.com>
2024-05-07 13:43:05 +0200

    rm leading and trailing spaces

--- data/companies/company_emission_calculations.py
+++ data/companies/company_emission_calculations.py
@@ -7,6 +7,9 @@ df = pd.read_excel("companyData.xlsx", sheet_name="SiteData")
 selected_columns = ['Company', 'URL 2023', 'Scope 1+2', 'Scope 3 (total)', 'Alex kommentar']
 selected_df = df[selected_columns]
 
+# Strip leading and trailing whitespaces from all string columns
+selected_df = selected_df.map(lambda x: x.strip() if isinstance(x, str) else x)
+
 # Rename the columns for clarity
 selected_df.columns = ['Company', 'URL', 'Scope1n2', 'Scope3', 'Comment']
 
