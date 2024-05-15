a14699136c6c97a12e07dd0cc6b227756c05100d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-05-16 01:04:10 +0200

    Remove old pulbic data file, and remove use of replaceLetters() since special characters for all names have already been replaced in the data file that is actually used.

--- utils/shared.ts
+++ utils/shared.ts
@@ -5,19 +5,4 @@ export const toTitleCase = (str: string) => str.replace(
   (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase(),
 )
 
-export const replaceLetters = (name: string): string => {
-  const replacements: Record<string, string> = {
-    'Ã¥': 'å',
-    'Ã¤': 'ä',
-    'Ã¶': 'ö',
-    'Ã…': 'Å',
-    'Ã„': 'Ä',
-    'Ã–': 'Ö',
-  }
-
-  const regex = new RegExp(Object.keys(replacements).join('|'), 'g')
-
-  return name.replace(regex, (match) => replacements[match])
-}
-
 export const ONE_WEEK_MS = 60 * 60 * 24 * 7
