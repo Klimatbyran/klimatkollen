2bdb057d02e40c6a7d5e07acb2ce87cfd6770b48
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-05-29 14:10:52 +0200

    Render ToggleButton with styled-components-wrapper to fix tests

--- __tests__/components/ToggleButton.test.tsx
+++ __tests__/components/ToggleButton.test.tsx
@@ -1,27 +1,28 @@
 import { render, fireEvent, screen } from '@testing-library/react'
 import ToggleButton from '../../components/ToggleButton'
+import StyledComponentsWrapper from '../utils/StyledComponentsWrapper'
 
 describe('ToggleButton', () => {
   it('should render the ToggleButton component', () => {
-    const { container } = render(<ToggleButton text="Test" icon={<span>Icon</span>} />)
+    const { container } = render(<ToggleButton text="Test" icon={<span>Icon</span>} />, { wrapper: StyledComponentsWrapper })
     expect(container).toBeInTheDocument()
   })
 
   it('should render the text provided', () => {
-    render(<ToggleButton text="Test" icon={<span>Icon</span>} />)
+    render(<ToggleButton text="Test" icon={<span>Icon</span>} />, { wrapper: StyledComponentsWrapper })
     const textElement = screen.getByText('Test')
     expect(textElement).toBeInTheDocument()
   })
 
   it('should render the icon provided', () => {
-    render(<ToggleButton text="Test" icon={<span>Icon</span>} />)
+    render(<ToggleButton text="Test" icon={<span>Icon</span>} />, { wrapper: StyledComponentsWrapper })
     const iconElement = screen.getByText('Icon')
     expect(iconElement).toBeInTheDocument()
   })
 
   it('should call handleClick when clicked', () => {
     const handleClick = vi.fn()
-    render(<ToggleButton text="Test" icon={<span>Icon</span>} handleClick={handleClick} />)
+    render(<ToggleButton text="Test" icon={<span>Icon</span>} handleClick={handleClick} />, { wrapper: StyledComponentsWrapper })
     const buttonElement = screen.getByRole('button')
     fireEvent.click(buttonElement)
     expect(handleClick).toHaveBeenCalled()
--- __tests__/utils/StyledComponentsWrapper.tsx
+++ __tests__/utils/StyledComponentsWrapper.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { StyleSheetManager } from 'styled-components'
 import isPropValid from '@emotion/is-prop-valid'
 
+import Theme from '../../Theme'
+
 /**
  * Make sure styled-components only render valid props to the DOM.
  */
 function StyledComponentsWrapper({ children }: { children: React.ReactNode }) {
   return (
     <StyleSheetManager shouldForwardProp={isPropValid}>
-      {children}
+      <Theme>
+        {children}
+      </Theme>
     </StyleSheetManager>
   )
 }
