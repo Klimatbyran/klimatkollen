574a5f44c6653a3c8c03249d9f9503363b2b1683
Mathias Bergqvist <mthbqv@gmail.com>
2024-05-31 23:47:21 +0200

    added CLI parameter -n to set number of decimals on all floats in output file

--- data/climate_data_calculations.py
+++ data/climate_data_calculations.py
@@ -2,7 +2,9 @@
 
 import argparse
 import json
+from typing import Any, Dict, List
 
+import numpy as np
 import pandas as pd
 
 from solutions.cars.electric_car_change_rate import get_electric_car_change_rate
@@ -49,36 +51,66 @@ def create_dataframe(to_percentage: bool) -> pd.DataFrame:
 
     return df
 
+def series_to_dict(row: pd.Series, numeric_columns: List[Any]) -> Dict:
+    """
+    Transforms a pandas Series into a dictionary.
 
-def convert_df_to_dict(df: pd.DataFrame, numeric_columns: list) -> dict:
-    temp = [
-        {
-            'kommun': df.iloc[i]['Kommun'],
-            'län': df.iloc[i]['Län'],
-            'emissions': { str(year): df.iloc[i][year] for year in numeric_columns },
-            'budget': df.iloc[i]['Budget'],
-            'emissionBudget': df.iloc[i]['parisPath'],
-            'approximatedHistoricalEmission': df.iloc[i]['approximatedHistorical'],
-            'totalApproximatedHistoricalEmission': df.iloc[i]['totalApproximatedHistorical'],
-            'trend': df.iloc[i]['trend'],
-            'trendEmission': df.iloc[i]['trendEmission'],
-            'historicalEmissionChangePercent': df.iloc[i]['historicalEmissionChangePercent'],
-            'neededEmissionChangePercent': df.iloc[i]['neededEmissionChangePercent'],
-            'hitNetZero': df.iloc[i]['hitNetZero'],
-            'budgetRunsOut': df.iloc[i]['budgetRunsOut'],
-            'electricCarChangePercent': df.iloc[i]['electricCarChangePercent'],
-            'electricCarChangeYearly': df.iloc[i]['electricCarChangeYearly'],
-            'climatePlanLink': df.iloc[i]['Länk till aktuell klimatplan'],
-            'climatePlanYear': df.iloc[i]['Antagen år'],
-            'climatePlanComment': df.iloc[i]['Namn, giltighetsår, kommentar'],
-            'bicycleMetrePerCapita': df.iloc[i]['metrePerCapita'],
-            'totalConsumptionEmission': df.iloc[i]['Total emissions'],
-            'electricVehiclePerChargePoints': df.iloc[i]['EVPC'],
-            'procurementScore': df.iloc[i]['procurementScore'],
-            'procurementLink': df.iloc[i]['procurementLink'],
-        }
-        for i in range(len(df))
-    ]
+    Args:
+    row: The pandas Series to transform.
+
+    Returns:
+    A dictionary with the transformed data.
+    """
+    cdata = {
+        'kommun': row['Kommun'],
+        'län': row['Län'],
+        'emissions': { str(year): row[year] for year in numeric_columns },
+        'budget': row['Budget'],
+        'emissionBudget': row['parisPath'],
+        'approximatedHistoricalEmission': row['approximatedHistorical'],
+        'totalApproximatedHistoricalEmission': row['totalApproximatedHistorical'],
+        'trend': row['trend'],
+        'trendEmission': row['trendEmission'],
+        'historicalEmissionChangePercent': row[
+            'historicalEmissionChangePercent'
+        ],
+        'neededEmissionChangePercent': row[
+            'neededEmissionChangePercent'
+        ],
+        'hitNetZero': row['hitNetZero'],
+        'budgetRunsOut': row['budgetRunsOut'],
+        'electricCarChangePercent': row['electricCarChangePercent'],
+        'electricCarChangeYearly': row['electricCarChangeYearly'],
+        'climatePlanLink': row['Länk till aktuell klimatplan'],
+        'climatePlanYear': row['Antagen år'],
+        'climatePlanComment': row['Namn, giltighetsår, kommentar'],
+        'bicycleMetrePerCapita': row['metrePerCapita'],
+        'totalConsumptionEmission': row['Total emissions'],
+        'electricVehiclePerChargePoints': row['EVPC'],
+        'procurementScore': row['procurementScore'],
+        'procurementLink': row['procurementLink'],
+    }
+    return cdata
+
+def round_processing(v, num_decimals: int):
+    new_v = v
+    if (isinstance(v, float)):
+        new_v = np.round(v, num_decimals)
+    elif (isinstance(v, dict)):
+        new_v = {k: round_processing(a, num_decimals) for k, a in v.items()}
+    return new_v
+
+def max_decimals(entry: Dict, num_decimals: int) -> Dict:
+    return {k: round_processing(v, num_decimals) for k, v in entry.items()}
+
+def df_to_dict(df: pd.DataFrame, num_decimals: int) -> dict:
+    numeric_columns = [col for col in df.columns if str(col).isdigit()]
+
+    if num_decimals >= 0:
+        temp = [ max_decimals(series_to_dict(df.iloc[i], numeric_columns), num_decimals) for i in range(len(df)) ]
+    else:
+        temp = [ series_to_dict(df.iloc[i], numeric_columns) for i in range(len(df)) ]
+    
     return temp
 
 
@@ -86,18 +118,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Climate data calculations")
     parser.add_argument("-o", "--outfile", default="output/climate-data.json", type=str, help="Output filename (JSON formatted)")
     parser.add_argument("-t", "--to_percentage", action="store_true", default=False, help="Convert to percentages")
+    parser.add_argument("-n", "--num_decimals", default=-1, type=int, help="Number of decimals to round to")
 
     args = parser.parse_args()
 
     df = create_dataframe(args.to_percentage)
 
-    # Save dataframe as JSON file
-    numeric_columns = [col for col in df.columns if str(col).isdigit()]
-
-    temp = convert_df_to_dict(df, numeric_columns)
+    temp = df_to_dict(df, args.num_decimals)
 
     output_file = args.outfile
 
+    # Save dataframe as JSON file
     with open(output_file, 'w', encoding='utf8') as json_file:
         # save dataframe as json file
         json.dump(temp, json_file, ensure_ascii=False, default=str)
