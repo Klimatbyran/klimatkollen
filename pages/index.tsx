0679f8608f87c6f957df69e1cbca06e1fea4220c
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-24 13:07:03 +0200

    Add notice

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import styled from 'styled-components'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
+import Icon from '../public/icons/boxedArrow.svg'
 import MetaTags from '../components/MetaTags'
 import { Company, Municipality, DatasetKey } from '../utils/types'
 import PageWrapper from '../components/PageWrapper'
@@ -16,12 +18,55 @@ import CompanyView from '../components/CompanyView'
 import PillSwitch from '../components/PillSwitch'
 import { DataView } from './[dataGroup]/[dataset]/[dataView]'
 import { ONE_WEEK_MS, normalizeString } from '../utils/shared'
+import Markdown from '../components/Markdown'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   align-items: center;
+`
+
+const NoticeContent = styled(Markdown)`
+  p {
+    margin: 0;
+    line-height: 1 !important;
+  }
+`
+
+const CompanyReportNotice = styled(Link)`
+  background: ${({ theme }) => theme.newColors.orange2};
+  color: ${({ theme }) => theme.newColors.black3};
+  padding: 0.5rem 0.75rem;
+  border-radius: 1rem;
+  text-decoration: none !important;
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 2rem;
+`
+
+const ArrowIcon = styled(Icon)`
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  z-index: 1;
+  margin: auto;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  fill: ${({ theme }) => theme.newColors.black3};
+`
+
+const Square = styled.div`
+  width: 24px;
+  height: 24px;
+  margin-top: 1px;
+  position: relative;
+  display: grid;
+  place-items: center;
+  border-radius: 4px;
 `
 
 export const defaultDataGroup = 'foretag'
@@ -71,6 +116,16 @@ function StartPage({ companies, municipalities, initialDataGroup }: PropsType) {
       />
       <PageWrapper compact>
         <Container>
+          <CompanyReportNotice href="/bolagsklimatkollen" target="_blank">
+            <NoticeContent>
+              **Läs vår rapport:** Bolagsklimatkollen - En analys av 150 svenska storbolags
+              klimatredovisning 2023
+            </NoticeContent>
+            <Square>
+              <ArrowIcon />
+            </Square>
+          </CompanyReportNotice>
+
           <PillSwitch
             selectedDataGroup={normalizedDataGroup}
             links={[
